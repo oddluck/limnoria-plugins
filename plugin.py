f34a4798dfb2e5f3e0b9e817d7e410bec9b48f77
James Scott <scottjab@gmail.com>
2012-12-12 23:46:22 -0500

    Added basic untested voting.

--- plugin.py
+++ plugin.py
@@ -34,6 +34,10 @@ import supybot.ircutils as ircutils
 import supybot.schedule as schedule
 import supybot.callbacks as callbacks
 
+from random import randint
+
+import operator 
+
 from cah import Game
 
 import time
@@ -45,6 +49,47 @@ class Cah(callbacks.Plugin):
 
     def __init__(self, irc):
         self.games = {}
+
+    ###### UTIL METHODS #######
+    def _msg(self, irc, channel, msg):
+        irc.queueMsg(ircmsgs.privmsg(channel, msg))
+    
+    def _printBlackCard(self, irc):
+        channel = self.channel 
+        try:
+            self._reply()
+        except KeyError:
+            self._msg("game not running.")
+
+
+    def _tallyVotes(self, votes):
+        talliedVotes = {}
+        for vote in votes.items():
+            try:
+                talliedVotes[vote] += 1
+            except KeyError:
+                talliedVotes[vote] = 1
+        #find ties
+
+        ties = []
+        winningCanidate = None
+        for canidate, count in votes.iteritems():
+            if winningCanidate == None:
+                winningCanidate = (canidate, votes)
+            elif winningCanidate[1] < count:
+                winningCanidate = (canidate, votes)
+                ties = []
+            elif winningCanidate[1] == count:
+                if len(ties) == 0:
+                    ties.append(winningCanidate)
+                ties.append((canidate, votes))
+            if len(ties) > 0:
+                return (ties[randint(0, len(ties) -1)], true)
+            else:
+
+            return (winningCanidate, False)
+
+
  
 
     ###### PRE GAME LOGIC ########
@@ -96,12 +141,49 @@ class Cah(callbacks.Plugin):
             channelGame['maxPlayers'] = 5
             channelGame['players']  = []
             self.games[channel] = channelGame
-            irc.reply("Who wants to play Cards Aganst Humanity?", prefixNick=False)
+            irc.reply("Who wants to play Cards Aganst Humanity?", prefix_nick=False)
             schedule.addEvent(startGame, time.time() + 60, "start_game_%s" % channel)
 
     ###### END PRE GAME LOGIC ######
 
 
+
+    ###### VOTING ##############
+
+
+    def startCardVote(self, irc, msg):
+        channel = ircutils.toLower(msg.args[0])
+        try:
+            game = self.games[channel]
+            game['votes'] = {}
+            self._printBlackCard(game)
+            self._printAnswers(game)
+            self._msg(irc, channel, "Please Vote on your favorite. @votecard <number> to vote, the entire channel can vote.")
+            schedule.addEvent(stopCardVote, time.time() + 60, "vote_%s" % channel)
+        except:
+            irc.reply("A Game is not running, or the time is not to vote.")
+
+    def votecard(self, irc, msg, vote):
+        channel = ircutils.toLower(msg.args[0])
+        try:
+            game = self.games[channel]
+            if msg.nick in game['votes'].keys():
+                irc.reply("You already voted! This isn't Chicago!")
+            else:
+                game['votes'][msg.nick] = vote
+                irc.reply("vote cast")
+        except:
+            irc.reply("A Game is not running, or the time is not to vote.")      
+
+    def stopCardVote(self, irc, msg):
+        channel = ircutils.toLower(msg.args[0])
+        try:
+            #TODO: NOt quite done here
+            game = self.games[channel]
+            winner = self._tallyVotes(game['votes'])
+            self._roundWinner(irc, winner)
+        except:
+            irc.reply("A Game is not running, or the time is not to vote.")
 Class = Cah
 
 
