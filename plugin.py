6a0253f9ec80dbc2144299014767e7e21351d478
spline <reticulatingspline@github.com>
2013-02-02 07:49:21 -0500

    Clean up whitespace. Add in exception for protected Tweets (user account is locked).

--- plugin.py
+++ plugin.py
@@ -34,32 +34,32 @@ class OAuthApi:
             token = None
         self._Consumer = oauth.Consumer(consumer_key, consumer_secret)
         self._signature_method = oauth.SignatureMethod_HMAC_SHA1()
-        self._access_token = token 
-        
+        self._access_token = token
+
     def _FetchUrl(self,url, parameters=None):
         extra_params = {}
         if parameters:
             extra_params.update(parameters)
-        
-        req = self._makeOAuthRequest(url, params=extra_params)      
+
+        req = self._makeOAuthRequest(url, params=extra_params)
         opener = urllib2.build_opener(urllib2.HTTPHandler(debuglevel=1))
         url = req.to_url()
         url_data = opener.open(url)
         opener.close()
         return url_data
-    
-    def _makeOAuthRequest(self, url, token=None, params=None):       
+
+    def _makeOAuthRequest(self, url, token=None, params=None):
         oauth_base_params = {
             'oauth_version': "1.0",
             'oauth_nonce': oauth.generate_nonce(),
             'oauth_timestamp': int(time.time())
             }
-        
+
         if params:
             params.update(oauth_base_params)
         else:
             params = oauth_base_params
-        
+
         if not token:
             token = self._access_token
         request = oauth.Request(method="GET", url=url, parameters=params)
@@ -81,7 +81,7 @@ class OAuthApi:
 class Tweety(callbacks.Plugin):
     """Public Twitter class for working with the API."""
     threaded = True
-    
+
     def __init__(self, irc):
         self.__parent = super(Tweety, self)
         self.__parent.__init__(irc)
@@ -100,11 +100,11 @@ class Tweety(callbacks.Plugin):
                     self.log.debug("Failed checking keys. We're missing the config value for: {0}. Please set this and try again.".format(checkKey))
                     failTest = True
                     break
-        
+
             if failTest:
                 self.log.error('Failed getting keys. You must set all 4 keys in config variables.')
                 return False
-        
+
             self.log.info("Got all 4 keys. Now trying to auth up with Twitter.")
             twitterApi = OAuthApi(self.registryValue('consumerKey'), self.registryValue('consumerSecret'), self.registryValue('accessKey'), self.registryValue('accessSecret'))
             data = twitterApi.ApiCall('account/verify_credentials')
@@ -143,7 +143,7 @@ class Tweety(callbacks.Plugin):
 
     def _time_created_at(self, s):
         """Return relative delta."""
-        
+
         try: # timeline's created_at Tue May 08 10:58:49 +0000 2012
             ddate = time.strptime(s, "%a %b %d %H:%M:%S +0000 %Y")[:-2]
         except ValueError:
@@ -158,7 +158,7 @@ class Tweety(callbacks.Plugin):
         # now parse and return.
         if d.days:
             rel_time = "%sd ago" % d.days
-        elif d.seconds > 3600: 
+        elif d.seconds > 3600:
             rel_time = "%sh ago" % (d.seconds / 3600)
         elif 60 <= d.seconds < 3600:
             rel_time = "%sm ago" % (d.seconds / 60)
@@ -170,35 +170,35 @@ class Tweety(callbacks.Plugin):
         """
         Takes a group of strings and outputs a Tweet to IRC. Used for tsearch and twitter.
         """
-        
+
         outputColorTweets = self.registryValue('outputColorTweets', msg.args[0])
-        
+
         if outputColorTweets:
             ret = ircutils.underline(ircutils.mircColor(("@" + nick), 'blue'))
         else:
             ret = ircutils.underline(ircutils.bold("@" + nick))
-            
+
         if not self.registryValue('hideRealName', msg.args[0]): # show realname in tweet output?
             ret += " ({0})".format(name)
-            
+
         # add in the end with the text + tape
         if outputColorTweets:
             text = re.sub(r'(http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)', ircutils.mircColor(r'\1', 'red'), text) # color urls.
             ret += ": {0} ({1})".format(text, ircutils.mircColor(time, 'yellow'))
-        else:            
+        else:
             ret += ": {0} ({1})".format(text, ircutils.bold(time))
-        
+
         if self.registryValue('addShortUrl', msg.args[0]):
             if self._createShortUrl(nick, tweetid):
                 ret += " {0}".format(url)
-        
+
         irc.reply(ret)
 
     def _createShortUrl(self, nick, tweetid):
         """
         Takes a nick and tweetid and returns a shortened URL via is.gd service.
         """
-        
+
         longurl = "https://twitter.com/#!/{0}/status/{1}".format(nick, tweetid)
         try:
             req = urllib2.Request("http://is.gd/api.php?longurl=" + urllib.quote(longurl))
@@ -214,11 +214,10 @@ class Tweety(callbacks.Plugin):
         """
 
         query = "select * from geo.places where text='%s'" % lookup
-        params = {
-                "q": query,
-                "format":"json",
-                "diagnostics":"false",
-                "env":"store://datatables.org/alltableswithkeys"}
+        params = {"q": query,
+                  "format":"json",
+                  "diagnostics":"false",
+                  "env":"store://datatables.org/alltableswithkeys"}
 
         try:
             response = urllib2.urlopen("http://query.yahooapis.com/v1/public/yql",urllib.urlencode(params))
@@ -232,7 +231,7 @@ class Tweety(callbacks.Plugin):
         except Exception, err:
             self.log.error("Error looking up %s :: %s" % (lookup,err))
             return None
-    
+
     ##########################
     ### PUBLIC FUNCTIONS #####
     ##########################
@@ -249,7 +248,7 @@ class Tweety(callbacks.Plugin):
             irc.reply(("Something broke while looking up: '%s'") % (lookup))
 
     woeidlookup = wrap(woeidlookup, ['text'])
-    
+
     def ratelimits(self, irc, msg, args):
         """
         Display current rate limits for your twitter API account.
@@ -259,23 +258,23 @@ class Tweety(callbacks.Plugin):
         if not self.twitterApi:
             irc.reply("ERROR: Twitter is not authorized. Please check logs before running this command.")
             return
-        
+
         data = self.twitterApi.ApiCall('application/rate_limit_status')
         try:
             data = json.loads(data.read())
         except:
             irc.reply("Failed to lookup ratelimit data: %s" % data)
-            return     
-        
+            return
+
         data = data.get('resources', None)
         if not data: # simple check if we have part of the json dict.
             irc.reply("Failed to fetch application rate limit status. Something could be wrong with Twitter.")
             self.log.error(data)
             return
 
-        # we only have resources needed in here. def below works with each entry properly.        
+        # we only have resources needed in here. def below works with each entry properly.
         resourcelist = ['trends/place', 'search/tweets', 'users/show/:id', 'statuses/show/:id', 'statuses/user_timeline']
-        
+
         for resource in resourcelist:
             family, endpoint = resource.split('/', 1) # need to split each entry on /, resource family is [0], append / to entry.
             resourcedict = data.get(family, None)
@@ -283,53 +282,53 @@ class Tweety(callbacks.Plugin):
             minutes = "%sm%ss" % divmod(int(endpoint['reset'])-int(time.time()), 60)
             output = "Reset in: {0}  Remaining: {1}".format(minutes, endpoint['remaining'])
             irc.reply("{0} :: {1}".format(ircutils.bold(resource), output))
-        
+
     ratelimits = wrap(ratelimits)
 
     def trends(self, irc, msg, args, getopts, optwoeid):
         """[--exclude] <location>
-        
-        Returns the Top 10 Twitter trends for a specific location. 
+
+        Returns the Top 10 Twitter trends for a specific location.
         Use optional argument location for trends. Ex: trends Boston
         Use --exclude to not include #hashtags in trends data.
         """
-        
+
         # before we do anything, make sure we have a twitterApi object.
         if not self.twitterApi:
             irc.reply("ERROR: Twitter is not authorized. Please check logs before running this command.")
             return
-        
+
         args = {'id':self.registryValue('woeid', msg.args[0]),'exclude':self.registryValue('hideHashtagsTrends', msg.args[0])}
         if getopts:
             for (key,value) in getopts:
                 if key=='exclude': # remove hashtags from trends.
                     args['exclude'] = 'hashtags'
-                
+
         # work with woeid. 1 is world, the default. can be set via input or via config.
         if optwoeid:
             woeid = self._woeid_lookup(optwoeid)
             if woeid:
                 args['id'] = woeid
-                  
+
         data = self.twitterApi.ApiCall('trends/place', parameters=args)
         try:
             data = json.loads(data.read())
         except:
             irc.reply("Error: failed to lookup Twitter trends: %s" % data)
             return
-                    
+
         try:
             location = data[0]['locations'][0]['name']
         except:
             irc.reply("ERROR: Cannot load trends: {0}".format(data)) # error also throws 404.
             return
-        
+
         # package together in object and output.
         ttrends = string.join([trend['name'].encode('utf-8') for trend in data[0]['trends']], " | ")
         irc.reply("Top 10 Twitter Trends in {0} :: {1}".format(ircutils.bold(location), ttrends))
 
     trends = wrap(trends, [getopts({'exclude':''}), optional('text')])
-    
+
     def tsearch(self, irc, msg, args, optlist, optterm):
         """[--num number] [--searchtype mixed,recent,popular] [--lang xx] <term>
 
@@ -342,7 +341,7 @@ class Tweety(callbacks.Plugin):
         if not self.twitterApi:
             irc.reply("ERROR: Twitter is not authorized. Please check logs before running this command.")
             return
-        
+
         tsearchArgs = {'include_entities':'false', 'count': self.registryValue('defaultSearchResults', msg.args[0]), 'lang':'en', 'q':urllib.quote(optterm)}
 
         if optlist:
@@ -357,7 +356,7 @@ class Tweety(callbacks.Plugin):
                 if key == 'searchtype':
                     tsearchArgs['result_type'] = value # limited by getopts to valid values.
                 if key == 'lang': # lang . Uses ISO-639 codes like 'en' http://en.wikipedia.org/wiki/ISO_639-1
-                    tsearchArgs['lang'] = value 
+                    tsearchArgs['lang'] = value
 
         data = self.twitterApi.ApiCall('search/tweets', parameters=tsearchArgs)
         try:
@@ -365,8 +364,8 @@ class Tweety(callbacks.Plugin):
         except:
             irc.reply("Error: %s trying to search Twitter." % data)
             return
-            
-        results = data.get('statuses', None) # data returned as a dict 
+
+        results = data.get('statuses', None) # data returned as a dict
 
         if not results or len(results) == 0:
             irc.reply("Error: No Twitter Search results found for '{0}'".format(optterm))
@@ -386,22 +385,22 @@ class Tweety(callbacks.Plugin):
         """[--noreply] [--nort] [--num number] <nick> | <--id id> | [--info nick]
 
         Returns last tweet or 'number' tweets (max 10). Shows all tweets, including rt and reply.
-        To not display replies or RT's, use --noreply or --nort, respectively. 
+        To not display replies or RT's, use --noreply or --nort, respectively.
         Or returns tweet with id 'id'.
-        Or returns information on user with --info. 
+        Or returns information on user with --info.
         """
 
         # before we do anything, make sure we have a twitterApi object.
         if not self.twitterApi:
             irc.reply("ERROR: Twitter is not authorized. Please check logs before running this command.")
             return
-            
+
         optnick = optnick.replace('@','') # strip @ from input if given.
-        
+
         args = {'id': False, 'nort': False, 'noreply': False, 'num': self.registryValue('defaultResults', msg.args[0]), 'info': False}
 
-        # handle input optlist. 
-        if optlist: 
+        # handle input optlist.
+        if optlist:
             for (key, value) in optlist:
                 if key == 'id':
                     args['id'] = True
@@ -412,14 +411,14 @@ class Tweety(callbacks.Plugin):
                 if key == 'num':
                     if value > self.registryValue('maxResults', msg.args[0]) or value <= 0:
                         irc.reply("Error: '{0}' is not a valid number of tweets. Range is above 0 and max {1}.".format(value, max))
-                        return                        
+                        return
                     else:
                         args['num'] = value
                 if key == 'info':
                     args['info'] = True
 
         # handle the three different rest api endpoint urls + twitterArgs dict for options.
-        if args['id']:  
+        if args['id']:
             apiUrl = 'statuses/show'
             twitterArgs = {'id': optnick, 'include_entities':'false'}
         elif args['info']:
@@ -432,7 +431,7 @@ class Tweety(callbacks.Plugin):
                 twitterArgs['include_rts'] = 'false'
             else:
                 twitterArgs['include_rts'] = 'true'
-                
+
             if args['noreply']: # This parameter will prevent replies from appearing in the returned timeline.
                 twitterArgs['exclude_replies'] = 'true'
             else:
@@ -445,8 +444,8 @@ class Tweety(callbacks.Plugin):
         except:
             irc.reply("Failed to lookup Twitter account for @{0} ({1}) ".format(optnick, data))
             return
-       
-        # process the data. 
+
+        # process the data.
         if args['id']: # If --id was given for a single tweet.
             text = self._unescape(data.get('text', None))
             nick = data["user"].get('screen_name', None)
@@ -464,7 +463,7 @@ class Tweety(callbacks.Plugin):
             name = data.get('name', None)
             url = data.get('url', None)
 
-            # build ret, output string    
+            # build ret, output string
             ret = ircutils.underline(ircutils.bold("@" + optnick))
             ret += " ({0}):".format(name.encode('utf-8'))
             if url:
@@ -473,14 +472,17 @@ class Tweety(callbacks.Plugin):
                 ret += " {0}".format(description.encode('utf-8'))
             ret += " [{0} friends,".format(ircutils.bold(friends))
             ret += " {0} followers.".format(ircutils.bold(followers))
-            if location: 
+            if location:
                 ret += " Location: {0}]".format(location.encode('utf-8'))
             else:
                 ret += "]"
-            
+
             irc.reply(ret)
             return
         else:
+            if data.has_key('error'):
+                irc.reply("ERROR: I cannot fetch tweets from {0}: {1}".format(optnick, data['error']))
+                return
             if len(data) == 0: # length handled above but user might have 0 tweets.
                 irc.reply("User: {0} has not tweeted yet.".format(optnick))
                 return
