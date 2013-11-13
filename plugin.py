dd2fb11d22b1f03657d4cece932afc3401bb7ee1
Th0masR0ss <Th0m4sR0ss@gmail.com>
2013-11-13 18:27:59 -0500

    Fix indentation error with a return instead of commenting it out.

--- plugin.py
+++ plugin.py
@@ -66,9 +66,9 @@ class TriviaTime(callbacks.Plugin):
         self.storage.makeEditTable()
         self.storage.makeInfoTable()
         #triviainfo table check
-        #if self.storage.isTriviaVersionSet():
-        #    if self.storage.getVersion() != None & self.storage.getVersion() != self.currentVersion:
-                #HERE IS WHERE WE UPDATE THE DATABASE.
+        if self.storage.isTriviaVersionSet():
+            if self.storage.getVersion() != None & self.storage.getVersion() != self.currentVersion:
+                return
 
     def doPrivmsg(self, irc, msg):
         """
