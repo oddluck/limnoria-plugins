838dbf35b373eb56c61baa592d87520c7853abd4
rootcoma <rootcoma@gmail.com>
2013-11-25 23:08:19 -0800

    more indenting

--- php/stats.php
+++ php/stats.php
@@ -101,129 +101,129 @@ include('config.php');
           ' and month=' . $week->format('n') .
           ' and year=' . $week->format('Y') . 
           ')';
-$week->add($interval);
-}
-?>
-<div class="span6">
-  <h2>Week Top Scores</h2>
-  <table class="table">
-    <thead>
-      <tr>
-        <th>#</th>
-        <th>Username</th>
-        <th>Points</th>
-      </tr>
-    </thead>
-    <tbody>
-      <?php
-      if ($db) {
-        $q = $db->query("SELECT username, sum(points_made) as points FROM triviauserlog WHERE $sqlClause GROUP BY username_canonical ORDER BY points DESC LIMIT 10");
-        if ($q === false) {
-          die("Error: database error: table does not exist\n");
-        } else {
-          $result = $q->fetchAll();
-          foreach($result as $key=>$res) {
-            echo '<tr>';
-            echo '<td>' . ($key+1) . '</td>';
-            echo '<td><a href="profile.php?username=' . $res['username'] . '">' . $res['username'] . '</a></td>';
-            echo '<td>' . number_format($res['points'],0) . '</td>';
-            echo '</tr>';
-          }
-        }
-      } else {
-        die($err);
+      $week->add($interval);
       }
       ?>
-
-    </tbody>
-  </table>
-</div>
-</div>
-<div class="row">
-  <div class="span6">
-    <h2>Month Top Scores</h2>
-    <table class="table">
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Username</th>
-          <th>Points</th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php
-        $day = date('j');
-        $month = date('m');
-        $year = date('Y');
-
-        if ($db) {
-          $q = $db->prepare("SELECT username, sum(points_made) as points FROM triviauserlog WHERE year=:year AND month=:month GROUP BY username_canonical ORDER BY points DESC LIMIT 10");
-          $q->execute(array(':year'=>$year, ':month'=>$month));
-          if ($q === false) {
-            die("Error: database error: table does not exist\n");
-          } else {
-            $result = $q->fetchAll();
-            foreach($result as $key=>$res) {
-              echo '<tr>';
-              echo '<td>' . ($key+1) . '</td>';
-              echo '<td><a href="profile.php?username=' . $res['username'] . '">' . $res['username'] . '</a></td>';
-              echo '<td>' . number_format($res['points'],0) . '</td>';
-              echo '</tr>';
+      <div class="span6">
+        <h2>Week Top Scores</h2>
+        <table class="table">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Username</th>
+              <th>Points</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+            if ($db) {
+              $q = $db->query("SELECT username, sum(points_made) as points FROM triviauserlog WHERE $sqlClause GROUP BY username_canonical ORDER BY points DESC LIMIT 10");
+              if ($q === false) {
+                die("Error: database error: table does not exist\n");
+              } else {
+                $result = $q->fetchAll();
+                foreach($result as $key=>$res) {
+                  echo '<tr>';
+                  echo '<td>' . ($key+1) . '</td>';
+                  echo '<td><a href="profile.php?username=' . $res['username'] . '">' . $res['username'] . '</a></td>';
+                  echo '<td>' . number_format($res['points'],0) . '</td>';
+                  echo '</tr>';
+                }
+              }
+            } else {
+              die($err);
             }
-          }
-        } else {
-          die($err);
-        }
-        ?>
-      </tbody>
-    </table>
-  </div>
-  <div class="span6">
-    <h2>Year Top Scores</h2>
-    <table class="table">
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Username</th>
-          <th>Points</th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php
-        $day = date('j');
-        $month = date('m');
-        $year = date('Y');
+            ?>
 
-        if ($db) {
-          $q = $db->prepare("SELECT username, sum(points_made) as points FROM triviauserlog WHERE year=:year GROUP BY username_canonical ORDER BY points DESC LIMIT 10");
-          $q->execute(array(':year'=>$year));
-          if ($q === false) {
-            die("Error: database error: table does not exist\n");
-          } else {
-            $result = $q->fetchAll();
-            foreach($result as $key=>$res) {
-              echo '<tr>';
-              echo '<td>' . ($key+1) . '</td>';
-              echo '<td><a href="profile.php?username=' . $res['username'] . '">' . $res['username'] . '</a></td>';
-              echo '<td>' . number_format($res['points'],0) . '</td>';
-              echo '</tr>';
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <div class="row">
+      <div class="span6">
+        <h2>Month Top Scores</h2>
+        <table class="table">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Username</th>
+              <th>Points</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+            $day = date('j');
+            $month = date('m');
+            $year = date('Y');
+
+            if ($db) {
+              $q = $db->prepare("SELECT username, sum(points_made) as points FROM triviauserlog WHERE year=:year AND month=:month GROUP BY username_canonical ORDER BY points DESC LIMIT 10");
+              $q->execute(array(':year'=>$year, ':month'=>$month));
+              if ($q === false) {
+                die("Error: database error: table does not exist\n");
+              } else {
+                $result = $q->fetchAll();
+                foreach($result as $key=>$res) {
+                  echo '<tr>';
+                  echo '<td>' . ($key+1) . '</td>';
+                  echo '<td><a href="profile.php?username=' . $res['username'] . '">' . $res['username'] . '</a></td>';
+                  echo '<td>' . number_format($res['points'],0) . '</td>';
+                  echo '</tr>';
+                }
+              }
+            } else {
+              die($err);
             }
-          }
-        } else {
-          die($err);
-        }
-        ?>
+            ?>
+          </tbody>
+        </table>
+      </div>
+      <div class="span6">
+        <h2>Year Top Scores</h2>
+        <table class="table">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Username</th>
+              <th>Points</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+            $day = date('j');
+            $month = date('m');
+            $year = date('Y');
 
-      </tbody>
-    </table>
-  </div>
-</div>
+            if ($db) {
+              $q = $db->prepare("SELECT username, sum(points_made) as points FROM triviauserlog WHERE year=:year GROUP BY username_canonical ORDER BY points DESC LIMIT 10");
+              $q->execute(array(':year'=>$year));
+              if ($q === false) {
+                die("Error: database error: table does not exist\n");
+              } else {
+                $result = $q->fetchAll();
+                foreach($result as $key=>$res) {
+                  echo '<tr>';
+                  echo '<td>' . ($key+1) . '</td>';
+                  echo '<td><a href="profile.php?username=' . $res['username'] . '">' . $res['username'] . '</a></td>';
+                  echo '<td>' . number_format($res['points'],0) . '</td>';
+                  echo '</tr>';
+                }
+              }
+            } else {
+              die($err);
+            }
+            ?>
 
-<div class="footer">
-  <p>&copy; Trivialand 2013 - <a target="_blank" href="https://github.com/tannn/TriviaTime">Github</a></p>
-</div>
+          </tbody>
+        </table>
+      </div>
+    </div>
 
-</div> <!-- /container -->
+    <div class="footer">
+      <p>&copy; Trivialand 2013 - <a target="_blank" href="https://github.com/tannn/TriviaTime">Github</a></p>
+    </div>
+
+  </div> <!-- /container -->
 
     <!-- Le javascript
     ================================================== -->
