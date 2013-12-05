4052eeaac3d1d008475c4be91e9a8f9ad60d2e95
rootcoma <rootcoma@gmail.com>
2013-12-04 20:21:02 -0800

    move logic to controller

--- php/top.php
+++ php/top.php
@@ -1,6 +1,8 @@
 <?php
 require_once('includes/autoload.php');
 
+$storage = $container->getStorage();
+
 $timespans = array('d'=>'Day', 'w'=>'Week', 'm'=>'Month', 'y'=>'Year');
 $timespan = 'd';
 $timeDesc = 'Day';
@@ -11,7 +13,51 @@ if(array_key_exists('t', $_GET)) {
   }
 }
 
+if(array_key_exists('page', $_GET)) {
+  $page = $_GET['page'];
+}
+if(!isset($page)) {
+  $page = 1;
+}
+if($page < 1) {
+  $page = 1;
+}
+
+$maxResults = 20;
+
+$resultCount = 0;
+$result = array();
+$errors = array();
+try {
+  if ($timespan == 'w') {
+    $result = $storage->getWeekTopScores($page, $maxResults);
+    $resultCount = $storage->getCountWeekTopScores();
+  } else if ($timespan == 'm') {
+    $result = $storage->getMonthTopScores($page, $maxResults);
+    $resultCount = $storage->getCountMonthTopScores();
+  } else if ($timespan == 'y') {
+    $result = $storage->getYearTopScores($page, $maxResults);
+    $resultCount = $storage->getCountYearTopScores();    
+  } else {
+    $result = $storage->getDayTopScores($page, $maxResults);
+    $resultCount = $storage->getCountDayTopScores();
+  }
+} catch(StorageSchemaException $e) {
+  $errors[] = "Error: Database schema is not queryable";
+} catch(StorageConnectionException $e) {
+  $errors[] = "Error: Database is not available";
+}
+
+$values = array();
+$values['result'] = $result;
+$values['resultCount'] = $resultCount;
+$values['maxResults'] = $maxResults;
+$values['page'] = $page;
+$values['timespan'] = $timespan;
+$values['timeDesc'] = $timeDesc;
+$values['errors'] = $errors;
+
 $container->setTitle('Top Scores for ' . $timeDesc);
 $container->setCurrentPage('top.php');
 
-$container->render('top.php');
+$container->render('top.php', $values);
--- php/views/top.php
+++ php/views/top.php
@@ -1,51 +1,3 @@
-<?php
-$storage = $container->getStorage();
-
-$timespans = array('d'=>'Day', 'w'=>'Week', 'm'=>'Month', 'y'=>'Year');
-$timespan = 'd';
-$timeDesc = 'Day';
-if(array_key_exists('t', $_GET)) {
-  if(array_key_exists(strtolower($_GET['t']), $timespans)) {
-    $timespan = strtolower($_GET['t']);
-    $timeDesc = $timespans[$timespan];
-  }
-}
-
-if(array_key_exists('page', $_GET)) {
-  $page = $_GET['page'];
-}
-if(!isset($page)) {
-  $page = 1;
-}
-if($page < 1) {
-  $page = 1;
-}
-
-$maxResults = 20;
-
-function replaceTimespanVariable($t) {
-    $pathInfo = parse_url($_SERVER['REQUEST_URI']);
-    if(array_key_exists('query', $pathInfo)) {
-        $queryString = $pathInfo['query'];
-    } else {
-        $queryString = '';
-    }
-    parse_str($queryString, $queryArray);
-    $queryArray['t'] = $t;
-    if($t == 'd') {
-        unset($queryArray['t']);
-    }
-    $queryArray['page'] = 1;
-    unset($queryArray['page']);
-    $queryString = http_build_query($queryArray);
-    $new = $pathInfo['path'];
-    if($queryString != ''){
-        $new .=  '?' . $queryString;
-    }
-    return $new;  
-}
-
-?>
     <div class="hero-unit">
       <h1>Top Scores</h1>
       <p>Player rankings.</p>
@@ -53,39 +5,20 @@ function replaceTimespanVariable($t) {
     <div class="row">
       <div class="span12">
         <ul class="nav nav-pills">
-          <li<?php if($timespan=='d') { echo ' class="active"'; } ?>><a href="<?php echo replaceTimespanVariable('d'); ?>">Day</a></li>
-          <li<?php if($timespan=='w') { echo ' class="active"'; } ?>><a href="<?php echo replaceTimespanVariable('w'); ?>">Week</a></li>
-          <li<?php if($timespan=='m') { echo ' class="active"'; } ?>><a href="<?php echo replaceTimespanVariable('m'); ?>">Month</a></li>
-          <li<?php if($timespan=='y') { echo ' class="active"'; } ?>><a href="<?php echo replaceTimespanVariable('y'); ?>">Year</a></li>
+          <li<?php if($values['timespan']=='d') { echo ' class="active"'; } ?>><a href="top.php?t=d">Day</a></li>
+          <li<?php if($values['timespan']=='w') { echo ' class="active"'; } ?>><a href="top.php?t=w">Week</a></li>
+          <li<?php if($values['timespan']=='m') { echo ' class="active"'; } ?>><a href="top.php?t=m">Month</a></li>
+          <li<?php if($values['timespan']=='y') { echo ' class="active"'; } ?>><a href="top.php?t=y">Year</a></li>
         </ul>
       </div>
     </div>
     <div class="row">
       <div class="span6">
-        <h2>Top Scores for <?php echo $timeDesc; ?></h2>
+        <h2>Top Scores for <?php echo $values['timeDesc']; ?></h2>
             <?php
-            $resultCount = 0;
-            $result = array();
-            try {
-              if ($timespan == 'w') {
-                $result = $storage->getWeekTopScores($page, $maxResults);
-                $resultCount = $storage->getCountWeekTopScores();
-              } else if ($timespan == 'm') {
-                $result = $storage->getMonthTopScores($page, $maxResults);
-                $resultCount = $storage->getCountMonthTopScores();
-              } else if ($timespan == 'y') {
-                $result = $storage->getYearTopScores($page, $maxResults);
-                $resultCount = $storage->getCountYearTopScores();    
-              } else {
-                $result = $storage->getDayTopScores($page, $maxResults);
-                $resultCount = $storage->getCountDayTopScores();
-              }
-            } catch(StorageSchemaException $e) {
-              echo "<div class='alert alert-error'>Error: Database schema is not queryable</div>";
-            } catch(StorageConnectionException $e) {
-              echo "<div class='alert alert-error'>Error: Database is not available</div>";
+            foreach($values['errors'] as $error) {
+              echo "<div class='alert alert-error'>$error</div>";
             }
-            $storage->close();
             ?>
         <table class="table">
           <thead>
@@ -97,8 +30,8 @@ function replaceTimespanVariable($t) {
           </thead>
           <tbody>
             <?php
-            $currentRank = ($page-1)*$maxResults + 1;
-            foreach($result as $res) {
+            $currentRank = ($values['page']-1)*$values['maxResults'] + 1;
+            foreach($values['result'] as $res) {
               echo '<tr>';
               echo '<td>' . $currentRank . '</td>';
               echo '<td><a href="profile.php?username=' . rawurlencode($res['username']) . '">' . $res['username'] . '</a></td>';
@@ -110,7 +43,7 @@ function replaceTimespanVariable($t) {
           </tbody>
         </table>
         <?php
-        $pagination = new Paginator($page, $resultCount, $maxResults); 
+        $pagination = new Paginator($values['page'], $values['resultCount'], $values['maxResults']); 
         $pagination->paginate(); 
         ?>
       </div>
