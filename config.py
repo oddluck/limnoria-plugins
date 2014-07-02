4a066fc41a6cc5b20eb90a40e361313581d8f091
wekeden <waratte.ka@gmail.com>
2014-07-02 02:00:47 -0400

    Update config.py

--- config.py
+++ config.py
@@ -56,7 +56,7 @@ conf.registerGlobalValue(Cobe, 'ignoreRegex', registry.String('^[.@!$%]', _("""R
 conf.registerGlobalValue(Cobe, 'stripUrls', registry.Boolean(True, _("""Keep the bot from learning URLs?""")))
 conf.registerGlobalValue(Cobe, 'stripNicks', registry.Boolean(True, _("""Strip all nicks, including the bots, when learning?""")))
 conf.registerChannelValue(Cobe, 'probability', registry.NonNegativeInteger(0, _("""Determines the percent of messages the bot will answer.""")))
-conf.registerChannelValue(Cobe, 'probabilityWhenAddressed', registry.NonNegativeInteger(100, _("""Determines the percent of messages adressed to the bot the bot will answer.""")))
+conf.registerChannelValue(Cobe, 'probabilityWhenAddressed', registry.NonNegativeInteger(10000, _("""Determines the percent of messages adressed to the bot the bot will answer, to the 0.01%. The maximum is 10000/10000""")))
 conf.registerChannelValue(Cobe, 'waitTimeBetweenSpeaking', registry.NonNegativeInteger(10, _("""Seconds to wait in a channel before speaking again.""")))
 conf.registerChannelValue(Cobe, 'ignoreWaitTimeIfAddressed', registry.Boolean(True, _("""If directly addressed, should we ignore the wait time?""")))
 conf.registerChannelValue(Cobe, 'responseDelay', registry.Boolean(True, _("""Delay responding for 2 to 4 seconds in order to seem more human?""")))
