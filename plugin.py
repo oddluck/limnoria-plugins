oddluck/limnoria-plugins

commit b5501e524eecb92debb2c3b1380d2ee223114cdc

Fix for hint command.
- Fix to prevent replies to the hint command after a KAOS question is over

plugin.py
@@ -1836,6 +1836,8 @@ class TriviaTime(callbacks.Plugin):
                     self.sendMessage(self.getOtherHintString())
 
         def getRemainingKAOS(self):
+            if self.questionOver:
+                return
             if len(self.answers) > 1:
                 if self.shownHint == False:
                     self.shownHint = True

commit 2f41a2f4370ee89a753b4fbdb57a7a8da22fee00

Minor fix for 'showreport' command.
- Bug introduced in b5501e5.

plugin.py
@@ -1310,9 +1310,9 @@ class TriviaTime(callbacks.Plugin):
                 irc.reply('Report #%d \'%s\' by %s on %s Q#%d '%(report['id'], report['report_text'], report['username'], report['channel'], report['question_num']))
                 question = threadStorage.getQuestionById(report['question_num'])
                 if question:
-                    irc.error('Question could not be found.')
-                else:
                     irc.reply('Question #%d: %s' % (question['id'], question['question']))
+                else:
+                    irc.error('Question could not be found.')
             else:
                 if self.registryValue('general.globalstats'):
                     irc.reply('Unable to find report #{0}.'.format(num))