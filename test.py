801891a7eb1fcc6c26e4c6d9a82d683496d8dadc
spline <reticulatingspline@github.com>
2014-10-11 12:56:09 -0400

    Lets test this.

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
-# -*- coding: utf-8 -*-
-# Copyright (c) 2013, spline
+# Copyright (c) 2013-2014, spline
 ###
 
 from supybot.test import *
+import os
 
 class TweetyTestCase(PluginTestCase):
     plugins = ('Tweety',)
 
+    def testTweety(self):
+        t = os.environ.get('test')
+        print t
 
 # vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
