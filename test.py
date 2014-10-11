5d32ce789a5b3779536a7a67fabdddfa8fad6980
spline <reticulatingspline@github.com>
2014-10-11 11:37:39 -0400

    Add in tests.

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 ###
-# Copyright (c) 2012-2013, spline
+# Copyright (c) 2012-2014, spline
 # All rights reserved.
 #
 #
@@ -9,6 +9,10 @@ from supybot.test import *
 
 class UrbanDictionaryTestCase(PluginTestCase):
     plugins = ('UrbanDictionary',)
+
+    def testUrbanDictionary(self):
+        conf.supybot.plugins.UrbanDictionary.disableANSI.setValue('True')
+        self.assertRegexp('urbandictionary hello', 'hello :: what you say when your talking casually with friends and your mom walks in the room')
 
 
 # vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
