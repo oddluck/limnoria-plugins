be845e6ff22bb59890f47f3538d03ab30b860dca
spline <reticulatingspline@github.com>
2014-10-11 13:10:36 -0400

    Lets test this.

--- test.py
+++ test.py
@@ -7,8 +7,20 @@ import os
 class TweetyTestCase(PluginTestCase):
     plugins = ('Tweety',)
 
-    def testTweety(self):
-        t = os.environ.get('test')
-        print t
+    def setUp(self):
+        PluginTestCase.setUp(self)
+        # get our variables via the secure environment.
+        consumerKey = os.environ.get('consumerKey')
+        consumerSecret = os.environ.get('consumerSecret')
+        accessKey = os.environ.get('accessKey')
+        accessSecret = os.environ.get('accessSecret')
+        # now set them.
+        conf.supybot.plugins.Tweety.consumerKey.setValue(consumerKey)
+        conf.supybot.plugins.Tweety.consumerSecret.setValue(consumerSecret)
+        conf.supybot.plugins.Tweety.accessKey.setValue(accessKey)
+        conf.supybot.plugins.Tweety.accessSecret.setValue(accessSecret)
 
+    def testTweety(self):
+        self.assertSnarfResponse('reload Tweety', 'The operation succeeded.')
+        self.assertRegex('trends', 'Top 10 Twitter Trends')
 # vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
