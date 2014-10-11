56883a4bf76317ef35b7d459e9ecec5197723546
spline <reticulatingspline@github.com>
2014-10-11 07:30:40 -0400

    Add in one more test.

--- test.py
+++ test.py
@@ -14,5 +14,6 @@ class WorldTimeTestCase(PluginTestCase):
         # New York, NY, USA :: Current local time is: Thu, 12:02 (Eastern Daylight Time)
         conf.supybot.plugins.WorldTime.disableANSI.setValue('True')
         self.assertRegexp('worldtime New York, NY', 'New York\, NY\, USA')
+        self.assertRegexp('worldtime Chicago', 'Current local time is')
 
 # vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
