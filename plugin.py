371a4b98dfced2531e8e971321ca424124150062
James Lu <glolol1@hotmail.com>
2014-12-22 16:02:52 -0800

    WorldTime: fix db handling, add 'worldtime --nick', changes to error syntax

--- plugin.py
+++ plugin.py
@@ -64,8 +64,8 @@ class WorldTime(callbacks.Plugin):
        """Flushes the (flatfile) database mapping ident@hosts to timezones."""
 
        try:
-           with open(filename, 'w') as f:
-               self.db = pickle.dumps(self.db, f)
+           with open(filename, 'wb') as f:
+               pickle.dump(self.db, f, 2)
        except Exception as e:
                self.log.warning('WorldTime: Unable to write pickled database: %s', e)
 
@@ -77,10 +77,10 @@ class WorldTime(callbacks.Plugin):
     ##################
     # TIME FUNCTIONS #
     ##################
-    
+
     def _utcnow(self):
         """Calculate Unix timestamp from GMT. Code from calendar.timegm()"""
-    
+
         ttuple = datetime.datetime.utcnow().utctimetuple()
         _EPOCH_ORD = datetime.date(1970, 1, 1).toordinal()
         year, month, day, hour, minute, second = ttuple[:6]
@@ -89,10 +89,10 @@ class WorldTime(callbacks.Plugin):
         minutes = hours*60 + minute
         seconds = minutes*60 + second
         return seconds
-    
+
     def _converttz(self, s, outputTZ):
         """Convert epoch seconds to a HH:MM readable string."""
-    
+
         # now do some timezone math.
         try:
             dtobj = datetime.datetime.fromtimestamp(s, tz=pytz.timezone(outputTZ)) # convert epoch into aware dtobj.
@@ -106,12 +106,12 @@ class WorldTime(callbacks.Plugin):
     ##############
     # GAPI STUFF #
     ##############
-    
+
     def _fetch(self, url, headers=None):
         """
         General HTTP resource fetcher.
         """
-        
+
         try:
             if headers:
                 result = utils.web.getUrl(url, headers=headers)
@@ -126,13 +126,13 @@ class WorldTime(callbacks.Plugin):
     def _getlatlng(self, location):
         location = quote_plus(location)
         url = 'http://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false' % location
-        
+
         # try and fetch url
         response = self._fetch(url)
         if not response:
             irc.error("I could not fetch: {0}".format(url), Raise=True)
             return None
-        
+
         # wrap in a big try/except
         try:
             result = json.loads(response.decode('utf-8'))
@@ -148,7 +148,7 @@ class WorldTime(callbacks.Plugin):
         except Exception as e:
             self.log.info("ERROR: _getlatlng: {0}".format(e))
             return None
-    
+
     def _gettime(self, latlng):
         latlng = quote_plus(latlng)
         url = 'https://maps.googleapis.com/maps/api/timezone/json?location=%s&sensor=false&timestamp=%s' % (latlng, time.time())
@@ -158,7 +158,7 @@ class WorldTime(callbacks.Plugin):
         if not response:
             irc.error("I could not fetch: {0}".format(url), Raise=True)
             return None
-        
+
         # wrap in a big try/except
         try:
             result = json.loads(response.decode('utf-8'))
@@ -166,60 +166,60 @@ class WorldTime(callbacks.Plugin):
                 # {u'status': u'OK', u'dstOffset': 0, u'rawOffset': -18000, u'timeZoneName': u'Eastern Standard Time', u'timeZoneId': u'America/New_York'}
                 return result
             else:
-                self.log.info("ERROR: _gettime: status result NOT ok. Result: {0}".format(result))
+                self.log.info("WorldTime: _gettime: status result NOT ok. Result: {0}".format(result))
                 return None
         except Exception as e:
-            self.log.info("ERROR: _gettime: {0}".format(e))
+            self.log.info("WorldTime: _gettime: {0}".format(e))
             return None
-    
+
     ###################
     # PUBLIC FUNCTION #
     ###################
-    
-    def worldtime(self, irc, msg, args, optinput):
-        """[<location>]
-        
+
+    def worldtime(self, irc, msg, args, opts, location):
+        """[--nick <nick] [<location>]
+
         Query GAPIs for <location> and attempt to figure out local time. [<location>]
         is only required if you have not yet set a location for yourself using the 'set'
-        command.
+        command. If --nick is given, try looking up the location for <nick>.
         """
-
-        if not optinput:
+        opts = dict(opts)
+        if not location:
             try:
-                ih = msg.prefix.split('!')[1]
-                optinput = self.db[ih]
+                if 'nick' in opts:
+                    host = irc.state.nickToHostmask(opts['nick'])
+                else:
+                    host = msg.prefix
+                ih = host.split('!')[1]
+                location = self.db[ih]
             except KeyError:
                 irc.error("No location for %s is set. Use the 'set' command "
                     "to set a location for your current hostmask, or call 'worldtime' "
                     "with <location> as an argument." % ircutils.bold('*!'+ih), Raise=True)
-        # first, grab lat and long for user location    
-        gc = self._getlatlng(optinput)
+        # first, grab lat and long for user location
+        gc = self._getlatlng(location)
         if not gc:
-            irc.error("I could not find lat/long for: {0}. Bad location? Spelled wrong?".format(optinput), Raise=True)
+            irc.error("I could not find the location for: {0}. Bad location? Spelled wrong?".format(location), Raise=True)
         # next, lets grab the localtime for that location w/lat+long.
         ll = self._gettime(gc['ll'])
         if not ll:
-            irc.error("I could not find local timezone for: {0}. Bad location? Spelled wrong?".format(optinput), Raise=True)
+            irc.error("I could not find the local timezone for: {0}. Bad location? Spelled wrong?".format(location), Raise=True)
         # if we're here, we have localtime zone.
         utcnow = self._utcnow()  # grab UTC now.
-        # localtm = utcnow+ll['rawOffset']  # grab raw offset from 
+        # localtm = utcnow+ll['rawOffset']  # grab raw offset from
         # now lets use pytz to convert into the localtime in the place.
         lt = self._converttz(utcnow, ll['timeZoneId'])
         if lt:  # make sure we get it back.
-            if self.registryValue('disableANSI', msg.args[0]):  # disable ANSI.
-                if sys.version_info[0] == 2:
-                    irc.reply("{0} :: Current local time is: {1} ({2})".format(gc['place'].encode('utf-8'), lt, ll['timeZoneName'].encode('utf-8')))
-                else:
-                    irc.reply("{0} :: Current local time is: {1} ({2})".format(gc['place'], lt, ll['timeZoneName']))
+            if sys.version_info[0] <= 2:
+                s = "{0} :: Current local time is: {1} ({2})".format(ircutils.bold(gc['place'].encode('utf-8')), lt, ll['timeZoneName'].encode('utf-8'))
             else:
-                if sys.version_info[0] == 2:
-                    irc.reply("{0} :: Current local time is: {1} ({2})".format(ircutils.bold(gc['place'].encode('utf-8')), lt, ll['timeZoneName'].encode('utf-8')))
-                else:
-                    irc.reply("{0} :: Current local time is: {1} ({2})".format(ircutils.bold(gc['place']), lt, ll['timeZoneName']))
+                s ="{0} :: Current local time is: {1} ({2})".format(ircutils.bold(gc['place']), lt, ll['timeZoneName'])
+            if self.registryValue('disableANSI', msg.args[0]):
+                s = ircutils.stripFormatting(s)
         else:
-            irc.error("Something went wrong during conversion to timezone. Check logs.", Raise=True)
+            irc.error("Something went wrong during conversion to timezone. Check the logs.", Raise=True)
 
-    worldtime = wrap(worldtime, [additional('text')])
+    worldtime = wrap(worldtime, [getopts({'nick': 'nick'}), additional('text')])
 
     def set(self, irc, msg, args, timezone):
         """<location>
