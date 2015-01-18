5c73598560e1fcb2ef4cc3ca303691fea7a174b0
Tanner <tannn3r@gmail.com>
2015-01-17 23:17:15 -0600

    Fixes bolding for kaos

--- plugin.py
+++ plugin.py
@@ -1729,7 +1729,7 @@ class TriviaTime(callbacks.Plugin):
 
                         # give a special message if it was KAOS
                         self.sendMessage('All KAOS answered! %s' % bonusPointsText)
-                        self.sendMessage('Total Awarded: \x02%d\x02 Points to %d\x02 Players\x02' % (int(self.totalAmountWon), len(self.correctPlayers)))
+                        self.sendMessage('Total Awarded: \x02%d\x02 Points to \x02%d\x02 Players' % (int(self.totalAmountWon), len(self.correctPlayers)))
 
                     self.removeEvent()
 
@@ -1928,7 +1928,7 @@ class TriviaTime(callbacks.Plugin):
                 if len(self.answers) > 1:
                     self.sendMessage( """Time's up! No one got \x02%s\x02""" % answer)
 
-                    self.sendMessage("""Correctly Answered: \x02%d\x02 of \x02%d\x02 Total Awarded: \x02%d\x02 Points to \x02%d Players\x02"""
+                    self.sendMessage("""Correctly Answered: \x02%d\x02 of \x02%d\x02 Total Awarded: \x02%d\x02 Points to \x02%d\x02 Players"""
                                     % (len(self.guessedAnswers), len(self.answers), int(self.totalAmountWon), len(self.correctPlayers))
                                     )
                 else:
