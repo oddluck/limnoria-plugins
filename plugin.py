ba59efff9e3a062b5fe2c657903a75f3bed977e6
liriel <liriel@sekrit.me>
2018-10-15 00:25:45 +0000

    Remove deprecated PTP tracker IP

--- plugin.py
+++ plugin.py
@@ -253,16 +253,16 @@ class Trackers(callbacks.Plugin):
 		content = WebParser().getWebData(irc,url)
 	
 		status = ([content["Website"], content["IRCTorrentAnnouncer"], content["IRCUserIdentifier"], 
-			     content["TrackerHTTPAddresses"]["51.255.35.82"], content["TrackerHTTPAddresses"]["51.255.35.90"],
+			     content["TrackerHTTPAddresses"]["51.255.35.90"],
 			     content["TrackerHTTPAddresses"]["164.132.51.73"], 
 			     content["TrackerHTTPAddresses"]["164.132.54.181"], content["TrackerHTTPAddresses"]["164.132.54.182"], 
 			     content["TrackerHTTPAddresses"]["192.99.58.220"],
 			     content["IRC"]])
 		status_headers = ([site_name+" Site","IRC Announce","IRC ID",
-						 "51.255.35.82", "51.255.35.90","164.132.51.73","164.132.54.181","164.132.54.182","192.99.58.220",
+						 "51.255.35.90","164.132.51.73","164.132.54.181","164.132.54.182","192.99.58.220",
 						 "Persona"])
-		breakpoints = [3,9]
-		line_headers = ["Services: ", "Trackers: ", "IRC: "]
+		breakpoints = [3]
+		line_headers = ["Services: ", "Trackers: "]
 
 		outStr = WebParser().prepareStatusString(site_name, status, status_headers,breakpoints,line_headers)
 
