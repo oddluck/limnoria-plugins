36262f11d179e68355879c1990e7ef90e985be1b
cottongin <cottongin@users.noreply.github.com>
2018-11-17 13:05:37 -0600

    test

--- plugin.py
+++ plugin.py
@@ -162,7 +162,7 @@ class CBBScores(callbacks.Plugin):
                     games[day][key] = {'short': short, 'long': long, 'ended': is_ended}
 
                 # sort events
-                games[day] = collections.OrderedDict(sorted(games[day].items(), key=games[day].get('ended'), reverse=False))
+                games[day] = collections.OrderedDict(sorted(games[day].items(), key=lambda k: k['ended'], reverse=True))
 
         return games
 
