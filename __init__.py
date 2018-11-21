7f0d60faf3bf91b555b75f91cf92a8635f203a2a
Gordon Shumway <39967334+oddluck@users.noreply.github.com>
2018-11-21 03:54:29 -0500

    make python3 compatible

--- __init__.py
+++ __init__.py
@@ -11,6 +11,7 @@ A feature-packed trivia plugin designed exclusively for #trivialand on Freenode.
 
 import supybot
 import supybot.world as world
+import imp
 
 # Use this for the version of this plugin.  You may wish to put a CVS keyword
 # in here if you're keeping the plugin in CVS or some similar system.
@@ -27,14 +28,14 @@ __contributors__ = {'rootcoma', 'Th0masR0ss', 'brrr'}
 # This is a url where the most recent plugin package can be downloaded.
 __url__ = 'https://github.com/tannn/TriviaTime/tree/master'
 
-import config
-import plugin
-reload(plugin) # In case we're being reloaded.
+from . import config
+from . import plugin
+imp.reload(plugin) # In case we're being reloaded.
 # Add more reloads here if you add third-party modules and want them to be
 # reloaded when this plugin is reloaded.  Don't forget to import them as well!
 
 if world.testing:
-    import test
+    from . import test
 
 Class = plugin.Class
 configure = config.configure
