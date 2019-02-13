a725c30652a4e276a675dbd5ca6b1a264dfc3bb3
Gordon Shumway <39967334+oddluck@users.noreply.github.com>
2019-02-13 12:18:58 -0500

    make python3 compatible

--- __init__.py
+++ __init__.py
@@ -19,17 +19,19 @@ __author__ = 'SpiderDave'
 # contributions.
 __contributors__ = {}
 
-import config
-import plugin
-reload(plugin) # In case we're being reloaded.
+from . import config
+from . import plugin
+from imp import reload
+reload(config) # In case we're being reloaded.
+reload(plugin)
 # Add more reloads here if you add third-party modules and want them to be
 # reloaded when this plugin is reloaded.  Don't forget to import them as well!
 
 if world.testing:
-    import test
+    from . import test
 
 Class = plugin.Class
-configure = config.configure
+Uno = config.Uno
 
 
 # vim:set shiftwidth=4 softtabstop=4 expandtab textwidth=79:
