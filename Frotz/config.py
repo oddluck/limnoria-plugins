190a6ec24d0d9ab95a26284827b798b14b2e887a
Gordon Shumway <39967334+oddluck@users.noreply.github.com>
2019-02-17 23:55:26 -0500

    Add files via upload

---
+++ Frotz/config.py
@@ -0,0 +1,28 @@
+###
+# Copyright (c) 2019, oddluck
+# All rights reserved.
+#
+#
+###
+
+import supybot.conf as conf
+import supybot.registry as registry
+try:
+    from supybot.i18n import PluginInternationalization
+    _ = PluginInternationalization('Frotz')
+except:
+    # Placeholder that allows to run the plugin on a bot
+    # without the i18n module
+    _ = lambda x: x
+
+
+def configure(advanced):
+    # This will be called by supybot to configure this module.  advanced is
+    # a bool that specifies whether the user identified themself as an advanced
+    # user or not.  You should effect your configuration by manipulating the
+    # registry as appropriate.
+    from supybot.questions import expect, anything, something, yn
+    conf.registerPlugin('Frotz', True)
+
+
+Frotz = conf.registerPlugin('Frotz')
---
+++ Frotz/test.py
@@ -0,0 +1,17 @@
+###
+# Copyright (c) 2019, oddluck
+# All rights reserved.
+#
+#
+###
+
+from supybot.test import *
+
+
+class InfocomTestCase(PluginTestCase):
+    plugins = ('Frotz',)
+
+
+# vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
+
+
