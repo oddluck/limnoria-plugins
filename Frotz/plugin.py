5e2c005ed8e303f8c9543cadecbd3ee8888f83a2
Gordon Shumway <39967334+oddluck@users.noreply.github.com>
2019-02-22 18:47:01 -0500

    various minor fixes

--- Frotz/plugin.py
+++ Frotz/plugin.py
@@ -41,13 +41,14 @@ class Frotz(callbacks.Plugin):
         Load <game_name.z*>.
         """
         channel = msg.args[0]
+        if not irc.isChannel(channel):
+            channel = msg.nick
         game_name = input
-        try: 
-            if self.game[channel]:
-                irc.reply("There is a game already in progress on {0}. Please end that game first.".format(channel))
-        except:
+        self.game.setdefault(channel, None)
+        if self.game[channel]:
+            irc.reply("There is a game already in progress on {0}. Please end that game first.".format(channel))
+        else:
             irc.reply("Starting {0} on {1}. Please wait...".format(game_name, channel))
-            self.game.setdefault(channel, None)
             game_file= "{0}{1}".format(self.game_path, game_name)
             self.game[channel] = pexpect.spawn("{0} -S 0 {1}".format(self.binary, game_file))
             response = self.output(self.game[channel])
@@ -58,11 +59,10 @@ class Frotz(callbacks.Plugin):
                 irc.reply(" ".join(response).replace(' .', '.'), prefixNick=False)
     load = wrap(load, ['text'])
 
-
     def output(self, output):
         response = []
         prompts = ["\n>", "\n> >", "to begin]", "\n\*\*\*MORE\*\*\*", pexpect.TIMEOUT]
-        output.expect(prompts, timeout=5)
+        output.expect(prompts, timeout=2)
         for line in output.before.splitlines():
             if line.strip():
                 response.append(line.decode().strip())
@@ -73,7 +73,10 @@ class Frotz(callbacks.Plugin):
         Stop game.
         """
         channel = msg.args[0]
-        try:
+        if not irc.isChannel(channel):
+            channel = msg.nick
+        self.game.setdefault(channel, None)
+        if self.game[channel]:
             if self.game[channel].isalive() is True:
                 irc.reply("Stopping Game. Thanks for playing.")
                 self.game[channel].close()
@@ -81,7 +84,7 @@ class Frotz(callbacks.Plugin):
                 del self.game[channel]
             else:
                 irc.reply("No game running in {0}".format(channel))
-        except:
+        else:
             irc.reply("No game started in {0}".format(channel))
     stop = wrap(stop)
 
@@ -90,11 +93,14 @@ class Frotz(callbacks.Plugin):
         Send user input or blank line (ENTER/RETURN) to the game.
         """
         channel = msg.args[0]
+        if not irc.isChannel(channel):
+            channel = msg.nick
+        self.game.setdefault(channel, None)
         if input:
             command = input.strip()
         else:
             command = None
-        try:
+        if self.game[channel]:
             if command:
                 self.game[channel].sendline(command)
             else:
@@ -102,10 +108,10 @@ class Frotz(callbacks.Plugin):
             response = self.output(self.game[channel])
             if len(response) > 2:
                 irc.reply(re.sub(' +', ' ', response[1]), prefixNick=False)
-                irc.reply(" ".join(response[2:].replace(' .', '.')), prefixNick=False)
+                irc.reply(" ".join(response[2:]).replace(' .', '.'), prefixNick=False)
             else:
                 irc.reply(" ".join(response).replace(' .', '.'), prefixNick=False)
-        except:
+        else:
             irc.reply("No game running in {0}?".format(channel))
     z = wrap(z, [additional('text')])
 
