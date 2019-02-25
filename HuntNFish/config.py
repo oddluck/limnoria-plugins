63eaa55e2c07bac5a0c5ece2cd821ca3acb8fc22
Gordon Shumway <39967334+oddluck@users.noreply.github.com>
2019-02-24 21:59:16 -0500

    set default success rate of 20%

--- HuntNFish/config.py
+++ HuntNFish/config.py
@@ -50,7 +50,7 @@ conf.registerChannelValue(HuntNFish, 'WeightType',
 conf.registerChannelValue(HuntNFish, 'enable',
     registry.Boolean(True, _("""Turns on and off the hunt and fish commands.""")))
 conf.registerChannelValue(HuntNFish, 'successRate',
-    registry.NonNegativeInteger(0, _("""Percent of chance of success""")))
+    registry.NonNegativeInteger(20, _("""Percent of chance of success""")))
 
 
 # vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
