98c090d3d09f381499e9349afc6c2858c05f6989
Gordon Shumway <39967334+oddluck@users.noreply.github.com>
2019-04-14 16:53:45 -0400

    python3 compatible

--- Timebomb/config.py
+++ Timebomb/config.py
@@ -46,99 +46,120 @@ Timebomb = conf.registerPlugin('Timebomb')
 #     registry.Boolean(False, """Help for someConfigVariableName."""))
 conf.registerGlobalValue(Timebomb, 'colors',
     registry.SpaceSeparatedListOfStrings(['AliceBlue', 'AntiqueWhite', 'Aqua',
-        'Aquamarine', 'Azure', 'Beige', 'Bisque', 'Black', 'BlanchedAlmond', 
+        'Aquamarine', 'Azure', 'Beige', 'Bisque', 'Black', 'BlanchedAlmond',
         'Blue', 'BlueViolet', 'Brown', 'BurlyWood', 'CadetBlue', 'Chartreuse',
         'Chocolate', 'Coral', 'CornflowerBlue', 'Cornsilk', 'Crimson', 'Cyan',
-        'DarkBlue', 'DarkCyan', 'DarkGoldenRod', 'DarkGray', 'DarkGreen', 
-        'DarkKhaki', 'DarkMagenta', 'DarkOliveGreen', 'DarkOrange', 
+        'DarkBlue', 'DarkCyan', 'DarkGoldenRod', 'DarkGray', 'DarkGreen',
+        'DarkKhaki', 'DarkMagenta', 'DarkOliveGreen', 'DarkOrange',
         'DarkOrchid', 'DarkRed', 'DarkSalmon', 'DarkSeaGreen', 'DarkSlateBlue',
-        'DarkSlateGray', 'DarkTurquoise', 'DarkViolet', 'DeepPink', 
+        'DarkSlateGray', 'DarkTurquoise', 'DarkViolet', 'DeepPink',
         'DeepSkyBlue', 'DimGray', 'DodgerBlue', 'FireBrick', 'FloralWhite',
-        'ForestGreen', 'Fuchsia', 'Gainsboro', 'GhostWhite', 'Gold', 
-        'GoldenRod', 'Gray', 'Green', 'GreenYellow', 'HoneyDew', 'HotPink', 
+        'ForestGreen', 'Fuchsia', 'Gainsboro', 'GhostWhite', 'Gold',
+        'GoldenRod', 'Gray', 'Green', 'GreenYellow', 'HoneyDew', 'HotPink',
         'IndianRed', 'Indigo', 'Ivory', 'Khaki', 'Lavender', 'LavenderBlush',
         'LawnGreen', 'LemonChiffon', 'LightBlue', 'LightCoral', 'LightCyan',
-        'LightGoldenRodYellow', 'LightGrey', 'LightGreen', 'LightPink', 
+        'LightGoldenRodYellow', 'LightGrey', 'LightGreen', 'LightPink',
         'LightSalmon', 'LightSeaGreen', 'LightSkyBlue', 'LightSlateGray',
-        'LightSteelBlue', 'LightYellow', 'Lime', 'LimeGreen', 'Linen', 
+        'LightSteelBlue', 'LightYellow', 'Lime', 'LimeGreen', 'Linen',
         'Magenta', 'Maroon', 'MediumAquaMarine', 'MediumBlue', 'MediumOrchid',
-        'MediumPurple', 'MediumSeaGreen', 'MediumSlateBlue', 
-        'MediumSpringGreen', 'MediumTurquoise', 'MediumVioletRed', 
-        'MidnightBlue', 'MintCream', 'MistyRose', 'Moccasin', 'NavajoWhite', 
-        'Navy', 'OldLace', 'Olive', 'OliveDrab', 'Orange', 'OrangeRed', 
-        'Orchid', 'PaleGoldenRod', 'PaleGreen', 'PaleTurquoise', 
-        'PaleVioletRed', 'PapayaWhip', 'PeachPuff', 'Peru', 'Pink', 'Plum', 
-        'PowderBlue', 'Purple', 'Red', 'RosyBrown', 'RoyalBlue', 
-        'SaddleBrown', 'Salmon', 'SandyBrown', 'SeaGreen', 'SeaShell', 
-        'Sienna', 'Silver', 'SkyBlue', 'SlateBlue', 'SlateGray', 'Snow', 
-        'SpringGreen', 'SteelBlue', 'Tan', 'Teal', 'Thistle', 'Tomato', 
-        'Turquoise', 'Violet', 'Wheat', 'White', 'WhiteSmoke', 'Yellow', 
+        'MediumPurple', 'MediumSeaGreen', 'MediumSlateBlue',
+        'MediumSpringGreen', 'MediumTurquoise', 'MediumVioletRed',
+        'MidnightBlue', 'MintCream', 'MistyRose', 'Moccasin', 'NavajoWhite',
+        'Navy', 'OldLace', 'Olive', 'OliveDrab', 'Orange', 'OrangeRed',
+        'Orchid', 'PaleGoldenRod', 'PaleGreen', 'PaleTurquoise',
+        'PaleVioletRed', 'PapayaWhip', 'PeachPuff', 'Peru', 'Pink', 'Plum',
+        'PowderBlue', 'Purple', 'Red', 'RosyBrown', 'RoyalBlue',
+        'SaddleBrown', 'Salmon', 'SandyBrown', 'SeaGreen', 'SeaShell',
+        'Sienna', 'Silver', 'SkyBlue', 'SlateBlue', 'SlateGray', 'Snow',
+        'SpringGreen', 'SteelBlue', 'Tan', 'Teal', 'Thistle', 'Tomato',
+        'Turquoise', 'Violet', 'Wheat', 'White', 'WhiteSmoke', 'Yellow',
         'YellowGreen'],
     """The set of possible timebomb wire colors"""))
 
-    
+
 conf.registerGlobalValue(Timebomb, 'shortcolors',
-        registry.SpaceSeparatedListOfStrings(['red', 'orange', 'yellow', 
-            'green', 'blue', 'purple', 'pink', 'black', 'brown', 'gray', 
+        registry.SpaceSeparatedListOfStrings(['red', 'orange', 'yellow',
+            'green', 'blue', 'purple', 'pink', 'black', 'brown', 'gray',
             'white'],
         """The set of possible timebomb wire colors when there are few
                 wires"""))
 
-conf.registerChannelValue(Timebomb, 'exclusions',
-        registry.SpaceSeparatedListOfStrings([], 
-        """A list of nicks who should be excluded from being 
+conf.registerChannelValue(Timebomb, 'randomExclusions',
+        registry.SpaceSeparatedListOfStrings([],
+        """A list of nicks who should be excluded from being
             randombombed"""))
 
-conf.registerChannelValue(Timebomb, 'allowBombs', 
-        registry.Boolean(False, """Determines whether timebombs are allowed 
+conf.registerChannelValue(Timebomb, 'exclusions',
+        registry.SpaceSeparatedListOfStrings([],
+        """A list of nicks who should be completely excluded from being
+            bombed"""))
+
+conf.registerChannelValue(Timebomb, 'allowBombs',
+        registry.Boolean(False, """Determines whether timebombs are allowed
             in the channel."""))
 
+conf.registerChannelValue(Timebomb, 'bombHistory',
+        registry.SpaceSeparatedListOfStrings([],
+        """Timestamps, senders and victims for previous bombs in the channel"""))
+
+conf.registerChannelValue(Timebomb, 'rateLimitTime',
+        registry.Integer(1800, """Time in seconds for which previous bombs are remembered and count towards the rate limit"""))
+
+conf.registerChannelValue(Timebomb, 'rateLimitSender',
+        registry.Float(5.0, """Mean bombs/hour allowed in the past rateLimitTime from each host"""))
+
+conf.registerChannelValue(Timebomb, 'rateLimitVictim',
+        registry.Float(3.0, """Mean bombs/hour allowed in the past rateLimitTime targeting a particular nick"""))
+
+conf.registerChannelValue(Timebomb, 'rateLimitTotal',
+        registry.Float(9.0, """Total mean bombs/hour allowed in the past rateLimitTime"""))
+
 conf.registerChannelValue(Timebomb, 'minWires',
-        registry.PositiveInteger(2, """Determines the minimum number of wires 
+        registry.PositiveInteger(2, """Determines the minimum number of wires
             a timebomb will have."""))
 
 conf.registerChannelValue(Timebomb, 'maxWires',
-        registry.PositiveInteger(4, """Determines the maximum number of wires 
+        registry.PositiveInteger(4, """Determines the maximum number of wires
             a timebomb will have."""))
 
 conf.registerChannelValue(Timebomb, 'minTime',
-        registry.PositiveInteger(45, """Determines the minimum time of a 
+        registry.PositiveInteger(45, """Determines the minimum time of a
             timebomb timer, in seconds."""))
 
 conf.registerChannelValue(Timebomb, 'maxTime',
-        registry.PositiveInteger(70, """Determines the maximum time of a 
+        registry.PositiveInteger(70, """Determines the maximum time of a
             timebomb timer, in seconds."""))
 
 conf.registerChannelValue(Timebomb, 'minRandombombTime',
-        registry.PositiveInteger(60, """Determines the minimum time of a 
-            randombomb timer, which should in general be greater than the 
-            minimum targeted bomb time, to allow someone who's not paying 
+        registry.PositiveInteger(60, """Determines the minimum time of a
+            randombomb timer, which should in general be greater than the
+            minimum targeted bomb time, to allow someone who's not paying
             attention to respond."""))
 
 conf.registerChannelValue(Timebomb, 'maxRandombombTime',
-        registry.PositiveInteger(120, """Determines the maximum time of a 
-            randombomb timer, which should in general be greater than the 
-            maxiumum targeted bomb time, to allow someone who's not paying 
+        registry.PositiveInteger(120, """Determines the maximum time of a
+            randombomb timer, which should in general be greater than the
+            maxiumum targeted bomb time, to allow someone who's not paying
             attention to respond."""))
 
 conf.registerChannelValue(Timebomb, 'showArt',
-        registry.Boolean(True, """Determines whether an ASCII art bomb should 
+        registry.Boolean(False, """Determines whether an ASCII art bomb should
             be shown on detonation, or a simple message."""))
 
 conf.registerChannelValue(Timebomb, 'bombActiveUsers',
-        registry.Boolean(False, """Determines whether only active users 
+        registry.Boolean(True, """Determines whether only active users
             should be randombombed"""))
 
 conf.registerChannelValue(Timebomb, 'joinIsActivity',
-        registry.Boolean(False, """Determines whether channel joins should 
+        registry.Boolean(False, """Determines whether channel joins should
             count as activity for randombombs"""))
 
 conf.registerChannelValue(Timebomb, 'allowSelfBombs',
         registry.Boolean(False, """Allow the bot to bomb itself?"""))
 
 conf.registerChannelValue(Timebomb, 'idleTime',
-        registry.PositiveInteger(60, """The number of minutes before someone 
-            is counted as idle for randombombs, if idle-checking is 
+        registry.PositiveInteger(30, """The number of minutes before someone
+            is counted as idle for randombombs, if idle-checking is
             enabled."""))
 
 conf.registerChannelValue(Timebomb, 'showCorrectWire',
