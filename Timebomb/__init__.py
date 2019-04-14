7e0a5a0b309618097701d61b863ee6eeed9f5f0a
Gordon Shumway <39967334+oddluck@users.noreply.github.com>
2019-04-14 16:51:38 -0400

    python3 compatible

--- Timebomb/__init__.py
+++ Timebomb/__init__.py
@@ -45,24 +45,27 @@ __version__ = ""
 
 # XXX Replace this with an appropriate author or supybot.Author instance.
 __author__ = supybot.Author('quantumlemur', 'quantumlemur',
-        'quantumlemur@users.sourceforge.net')
+                            'quantumlemur@users.sourceforge.net')
 
 # This is a dictionary mapping supybot.Author instances to lists of
 # contributions.
 __contributors__ = {}
 
 # This is a url where the most recent plugin package can be downloaded.
-__url__ = '' # 'http://supybot.com/Members/yourname/Timebomb/download'
+__url__ = ''  # 'http://supybot.com/Members/yourname/Timebomb/download'
 
-import config
-import plugin
-reload(plugin) # In case we're being reloaded.
+from . import config
+from . import plugin
+from imp import reload
+# In case we're being reloaded.
+reload(config)
+reload(plugin)
 # Add more reloads here if you add third-party modules and want them to be
 # reloaded when this plugin is reloaded.  Don't forget to import them as well!
 
 
 if world.testing:
-    import test
+    from . import test
 
 Class = plugin.Class
 configure = config.configure
