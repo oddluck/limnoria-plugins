40af62c082d7e6da327d01949f373be55f92379c
Gordon Shumway <39967334+oddluck@users.noreply.github.com>
2019-04-26 04:56:55 -0400

    remove randwait

--- ASCII/config.py
+++ ASCII/config.py
@@ -33,7 +33,4 @@ conf.registerChannelValue(ASCII, 'pasteEnable',
 registry.Boolean(False, _("""Turns on and off paste.ee support""")))
 
 conf.registerChannelValue(ASCII, 'delay',
-registry.Float(1.0, _("""Set the time delay betwen lines""")))
-
-conf.registerChannelValue(ASCII, 'randWait',
-registry.CommaSeparatedListOfStrings(["Please be patient while I render the image into ASCII characters and colorize the output", "Please be patient while I render the image into ASCII characters and colorize the output"], _("""Comma seperated list of wait messages""")))
+registry.Float(1.0, _("""Set the time delay betwen lines. Not currently implemented.""")))
