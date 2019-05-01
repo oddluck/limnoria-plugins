8b561121029f1e4069408038a82aa047eff0e6ed
Gordon Shumway <39967334+oddluck@users.noreply.github.com>
2019-05-01 03:37:37 -0400

    add chars option to img wrap

--- ASCII/plugin.py
+++ ASCII/plugin.py
@@ -415,7 +415,7 @@ class ASCII(callbacks.Plugin):
             except:
                 return
                 #irc.reply("Error. Did you set a valid Paste.ee API Key? https://paste.ee/account/api")
-    img = wrap(img,[getopts({'w':'int', 'invert':'', 'fast':'', 'faster':'', 'slow':'', 'slower':'', 'slowest':'', 'insane':'', '16':'', 'delay':'float', 'dither':''}), ('text')])
+    img = wrap(img,[getopts({'w':'int', 'invert':'', 'fast':'', 'faster':'', 'slow':'', 'slower':'', 'slowest':'', 'insane':'', '16':'', 'delay':'float', 'dither':'', 'chars':'text'}), ('text')])
 
     def ansi(self, irc, msg, args, optlist, url):
         """[--w <width>] [--16] <url>
@@ -730,7 +730,7 @@ class ASCII(callbacks.Plugin):
                     #irc.reply("Error. Did you set a valid Paste.ee API Key? https://paste.ee/account/api")
         else:
             irc.reply("Unexpected file type or link format", private=False, notice=False)
-    p2u = wrap(p2u, [getopts({'b':'int', 'f':'text', 'p':'text', 's':'int', 't':'int', 'w':'int', 'delay':'float', 'chars':'text'}), ('text')])
+    p2u = wrap(p2u, [getopts({'b':'int', 'f':'text', 'p':'text', 's':'int', 't':'int', 'w':'int', 'delay':'float'}), ('text')])
 
 
     def tdf(self, irc, msg, args, optlist, text):
