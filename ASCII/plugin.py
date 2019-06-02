5ee0b0cd340eedd0854208b850276b0d3fb91ece
Gordon Shumway <39967334+oddluck@users.noreply.github.com>
2019-06-02 19:26:21 -0400

    1/2 block regex change

--- ASCII/plugin.py
+++ ASCII/plugin.py
@@ -633,7 +633,8 @@ class ASCII(callbacks.Plugin):
                             aimg[k] += " "
                         else:
                             aimg[k] += "▀"
-                    aimg[k] = re.sub("(\x03\d,{0}(\s+)\x03(\d+,){0})".format(color.split(',')[1]), "\x03\g<3>{0}\g<2>".format(color.split(',')[1]), aimg[k])
+                for i in range(0,98)
+                    aimg[k] = re.sub("\x03\d,{0}(\s+)\x03(\d+,){0}|\x03\d\d,{0}(\s+)\x03(\d+,){0}".format(i), "\x03\g<2>{0}\g<1>".format(i), aimg[k])
                 k += 1
         elif '1/4' in optlist:
             k = 0
