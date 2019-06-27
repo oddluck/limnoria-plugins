29c85eb526897ce87bceb86618254043be5badf4
Gordon Shumway <39967334+oddluck@users.noreply.github.com>
2019-06-26 23:17:04 -0400

    add options

--- ASCII/config.py
+++ ASCII/config.py
@@ -37,3 +37,21 @@ registry.Boolean(False, _("""Turns on and off paste.ee support""")))
 
 conf.registerChannelValue(ASCII, 'delay',
 registry.Float(1.0, _("""Set the time delay betwen lines. Not currently implemented.""")))
+
+conf.registerChannelValue(ASCII, 'dither',
+registry.Boolean(True, _("""Enable dithering. Results in much faster rendering at a slight decrease in quality. Default: True""")))
+
+conf.registerChannelValue(ASCII, 'speed',
+registry.String('Slow', _("""Set the speed of the color rendering. 'Slow' (default) to use CIEDE2000 color difference. 'Fast' to use Euclidean color difference.""")))
+
+conf.registerChannelValue(ASCII, 'imgDefault',
+registry.String('1/2', _("""Set the default art type for the img command. Options are 'ascii', '1/2' (default), '1/4', 'block', and 'no-color'""")))
+
+conf.registerChannelValue(ASCII, 'asciiWidth',
+registry.Integer(100, _("""Set the default column width for ascii art images""")))
+
+conf.registerChannelValue(ASCII, 'blockWidth',
+registry.Integer(70, _("""Set the default column width for 1/2 and 1/4 block art images""")))
+
+conf.registerChannelValue(ASCII, 'colors',
+registry.Integer(99, _("""Set the default number of colors to use. Options are 16 for colors 0-15 only, 83 for colors 16-98 only, and 99 (default) to use all available colors""")))
