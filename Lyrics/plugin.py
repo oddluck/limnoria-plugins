35172b4815a05a817fede96bbbe1565fa024cb94
Gordon Shumway <39967334+oddluck@users.noreply.github.com>
2019-11-15 20:19:28 -0500

    Update plugin.py

--- Lyrics/plugin.py
+++ Lyrics/plugin.py
@@ -11,11 +11,8 @@ import supybot.plugins as plugins
 import supybot.ircutils as ircutils
 import supybot.callbacks as callbacks
 import supybot.ircmsgs as ircmsgs
-from bs4 import BeautifulSoup
-import requests
 import re
 import pylyrics3
-from fake_useragent import UserAgent
 
 try:
     from supybot.i18n import PluginInternationalization
@@ -29,49 +26,21 @@ class Lyrics(callbacks.Plugin):
     """Retrieves song lyrics"""
     threaded = True
 
-    def dosearch(self, lyric):
-        try:
-            searchurl = "https://www.google.com/search?&q={0} site:lyrics.wikia.com/wiki \"Lyrics\"".format(lyric)
-            ua = UserAgent()
-            header = {'User-Agent':str(ua.random)}
-            data = requests.get(searchurl, headers=header)
-            soup = BeautifulSoup(data.text)
-            elements = soup.select('.r a')
-            url = elements[0]['href']
-            url = re.split('https?://', url)[-1]
-            url = re.sub("&rct=.*", "", url)
-            url = "https://{0}".format(url)
-            title = soup.find("h3").getText()
-        except Exception:
-            return
-        else:
-            return title, url
-
-    def getlyrics(self, url):
-        try:
-            lyrics = pylyrics3.get_lyrics_from_url(url)
-            lyrics = re.sub('(?<!\.|\!|\?)\s\\n', '.', lyrics).replace(" \n", "")
-        except Exception:
-            lyrics = pylyrics3.get_lyrics_from_url(url)
-            lyrics = re.sub('(?<!\.|\!|\?)\s\\n', '.', lyrics).replace(" \n", "")
-        else:
-            return lyrics
-
     def lyric(self, irc, msg, args, lyric):
         """<text>
-        Get song lyrics from Lyrics Wiki. Search powered by Google.
+        Get song lyrics from Lyrics Wiki. Format searches as artist, song name
         """
-        try:
-            title, url = self.dosearch(lyric)
-        except Exception:
-            irc.reply("No results found for {0}".format(lyric))
+        if ',' in lyric:
+            lyric = lyric.split(',')
         else:
-            try:
-                lyrics = self.getlyrics(url)
-                irc.reply(title.replace(":", " - "))
-                irc.reply(lyrics)
-            except Exception:
-                irc.reply("Unable to retrieve lyrics from {0}".format(url))
+            irc.reply("Lyric searches must be formatted as artist, song name")
+            return
+        lyrics = pylyrics3.get_song_lyrics(lyric[0].strip(), lyric[1].strip())
+        lyrics = re.sub('(?<!\.|\!|\?)\s\\n', '.', lyrics).replace(" \n", "")
+        if lyrics != 'None':
+            irc.reply(lyrics)
+        else:
+            irc.reply("No results found")
     lyric = wrap(lyric, ['text'])
 
 Class = Lyrics
