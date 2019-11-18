156e0c76495f4b7237ca6823a3c837173f3fe574
Gordon Shumway <39967334+oddluck@users.noreply.github.com>
2019-11-18 01:10:22 -0500

    add rot/unrot

--- Fun/plugin.py
+++ Fun/plugin.py
@@ -270,5 +270,15 @@ class Fun(callbacks.Plugin):
         excuse = random.randrange(0, len(reply))
         irc.reply(reply[excuse])
     chuck = wrap(chuck)
+    
+    def rot(self, irc, msg, args, text):
+        "Encode text with ROT13"
+        irc.reply(codecs.encode(text, "rot_13"))
+    rot = wrap(rot, ['text'])
+
+    def unrot(self, irc, msg, args, text):
+        "Decode ROT13 text"
+        irc.reply(codecs.decode(text, "rot_13"))
+    unrot = wrap(unrot, ['text'])
 
 Class = Fun
