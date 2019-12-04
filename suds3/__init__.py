cef32067a8778fd7a1298d043fa07a842c9be319
Gordon Shumway <39967334+oddluck@users.noreply.github.com>
2019-12-04 02:40:58 -0500

    update author/maintainer data

--- suds3/__init__.py
+++ suds3/__init__.py
@@ -25,17 +25,19 @@ import importlib
 
 # Use this for the version of this plugin.  You may wish to put a CVS keyword
 # in here if you're keeping the plugin in CVS or some similar system.
-__version__ = "SUDS"
+__version__ = ""
 
 # XXX Replace this with an appropriate author or supybot.Author instance.
 __author__ = supybot.Author('Taede Werkhoven', 'TWerkhoven', 't.werkhoven@turbulent-t.com')
+__maintainer__ = getattr(supybot.authors, 'oddluck',
+                         supybot.Author('oddluck', 'oddluck', 'oddluck@riseup.net'))
 
 # This is a dictionary mapping supybot.Author instances to lists of
 # contributions.
 __contributors__ = {}
 
 # This is a url where the most recent plugin package can be downloaded.
-__url__ = 'https://github.com/luaduck/suds'
+__url__ = 'https://github.com/oddluck/limnoria-plugins/'
 
 from . import config
 from . import enums
