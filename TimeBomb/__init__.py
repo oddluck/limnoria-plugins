a3ff1ad679e1faabebc7cd3ebbbea712e0b984e3
Gordon Shumway <39967334+oddluck@users.noreply.github.com>
2019-12-04 02:05:06 -0500

    update author/maintainer data

--- TimeBomb/__init__.py
+++ TimeBomb/__init__.py
@@ -46,13 +46,15 @@ __version__ = ""
 # XXX Replace this with an appropriate author or supybot.Author instance.
 __author__ = supybot.Author('quantumlemur', 'quantumlemur',
                             'quantumlemur@users.sourceforge.net')
+__maintainer__ = getattr(supybot.authors, 'oddluck',
+                         supybot.Author('oddluck', 'oddluck', 'oddluck@riseup.net'))
 
 # This is a dictionary mapping supybot.Author instances to lists of
 # contributions.
 __contributors__ = {}
 
 # This is a url where the most recent plugin package can be downloaded.
-__url__ = ''  # 'http://supybot.com/Members/yourname/TimeBomb/download'
+__url__ = 'https://github.com/oddluck/limnoria-plugins/'
 
 from . import config
 from . import plugin
