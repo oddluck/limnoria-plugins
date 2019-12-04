c75ed6e0212319444fdd34296da02ff336c1e5a2
Gordon Shumway <39967334+oddluck@users.noreply.github.com>
2019-12-04 00:49:39 -0500

    update author/maintainer data

--- BadWords/__init__.py
+++ BadWords/__init__.py
@@ -39,11 +39,18 @@ import supybot.world as world
 # in here if you're keeping the plugin in CVS or some similar system.
 __version__ = ""
 
+# XXX Replace this with an appropriate author or supybot.Author instance.
 __author__ = supybot.authors.jemfinch
+__maintainer__ = getattr(supybot.authors, 'oddluck',
+                         supybot.Author('oddluck', 'oddluck', 'oddluck@riseup.net'))
+
 
 # This is a dictionary mapping supybot.Author instances to lists of
 # contributions.
 __contributors__ = {}
+
+# This is a url where the most recent plugin package can be downloaded.
+__url__ = 'https://github.com/oddluck/limnoria-plugins/'
 
 from . import config
 from . import plugin
