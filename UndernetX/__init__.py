3957ab7392a443cafe2ad6da2a0e62dbf855343c
Gordon Shumway <39967334+oddluck@users.noreply.github.com>
2019-12-04 02:12:33 -0500

    update author/maintainer data

--- UndernetX/__init__.py
+++ UndernetX/__init__.py
@@ -29,8 +29,7 @@
 ###
 
 """
-Add a description of the plugin (to be presented to the user inside the wizard)
-here.  This should describe *what* the plugin does.
+Undernet Utilities
 """
 
 import supybot
@@ -39,17 +38,19 @@ import importlib
 
 # Use this for the version of this plugin.  You may wish to put a CVS keyword
 # in here if you're keeping the plugin in CVS or some similar system.
-__version__ = "0.1"
+__version__ = ""
 
 # XXX Replace this with an appropriate author or supybot.Author instance.
 __author__ = supybot.Author("Ken Spencer", "kspencer", "ken@electrocode.net")
+__maintainer__ = getattr(supybot.authors, 'oddluck',
+                         supybot.Author('oddluck', 'oddluck', 'oddluck@riseup.net'))
 
 # This is a dictionary mapping supybot.Author instances to lists of
 # contributions.
 __contributors__ = {}
 
 # This is a url where the most recent plugin package can be downloaded.
-__url__ = '' # 'http://supybot.com/Members/yourname/UndernetX/download'
+__url__ = 'https://github.com/oddluck/limnoria-plugins/'
 
 from . import config
 from . import plugin
