a897fce29a1b3b937435c82c3e57e49a64ba0346
Gordon Shumway <39967334+oddluck@users.noreply.github.com>
2019-12-05 00:02:28 -0500

    Add files via upload

---
+++ NFLScores/test.py
@@ -0,0 +1,15 @@
+###
+# Copyright (c) 2019, cottongin
+# All rights reserved.
+#
+#
+###
+
+from supybot.test import *
+
+
+class NFLScoresTestCase(PluginTestCase):
+    plugins = ('NFLScores',)
+
+
+# vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
