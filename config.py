ff4291e42dbcf3f37adf152956de70ff513175db
oddluck <39967334+oddluck@users.noreply.github.com>
2019-12-19 07:56:04 +0000

    Add RedditBot: a markov chat plugin.

--- config.py
+++
@@ -1,9 +0,0 @@
-"""Required constants for the Reddit API."""
-
-# The following constants are used by the bot.
-REDDIT_USERNAME = ""
-REDDIT_PASSWORD = ""
-
-APP_ID = ""
-APP_SECRET = ""
-USER_AGENT = ""
