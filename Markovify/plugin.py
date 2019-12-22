273a154f4c07caddd89c14ea6f78d84cb378e123
oddluck <39967334+oddluck@users.noreply.github.com>
2019-12-22 03:50:07 +0000

    Markovify: add/improve imports/filtering.

--- Markovify/plugin.py
+++ Markovify/plugin.py
@@ -13,6 +13,7 @@ import supybot.callbacks as callbacks
 import supybot.ircmsgs as ircmsgs
 import supybot.log as log
 import supybot.conf as conf
+from itertools import chain
 import os
 import requests
 import random
@@ -21,6 +22,7 @@ import json
 import markovify
 import spacy
 from ftfy import fix_text
+import gc
 
 try:
     from supybot.i18n import PluginInternationalization
@@ -31,6 +33,132 @@ except ImportError:
     _ = lambda x: x
 
 nlp = spacy.load('en_core_web_sm')
+
+CONTRACTION_MAP = {
+"ain't": "is not",
+"aren't": "are not",
+"can't": "cannot",
+"can't've": "cannot have",
+"'cause": "because",
+"could've": "could have",
+"couldn't": "could not",
+"couldn't've": "could not have",
+"didn't": "did not",
+"doesn't": "does not",
+"don't": "do not",
+"hadn't": "had not",
+"hadn't've": "had not have",
+"hasn't": "has not",
+"haven't": "have not",
+"he'd": "he would",
+"he'd've": "he would have",
+"he'll": "he will",
+"he'll've": "he he will have",
+"he's": "he is",
+"how'd": "how did",
+"how'd'y": "how do you",
+"how'll": "how will",
+"how's": "how is",
+"I'd": "I would",
+"I'd've": "I would have",
+"I'll": "I will",
+"I'll've": "I will have",
+"I'm": "I am",
+"I've": "I have",
+"i'd": "i would",
+"i'd've": "i would have",
+"i'll": "i will",
+"i'll've": "i will have",
+"i'm": "i am",
+"i've": "i have",
+"isn't": "is not",
+"it'd": "it would",
+"it'd've": "it would have",
+"it'll": "it will",
+"it'll've": "it will have",
+"it's": "it is",
+"let's": "let us",
+"ma'am": "madam",
+"mayn't": "may not",
+"might've": "might have",
+"mightn't": "might not",
+"mightn't've": "might not have",
+"must've": "must have",
+"mustn't": "must not",
+"mustn't've": "must not have",
+"needn't": "need not",
+"needn't've": "need not have",
+"o'clock": "of the clock",
+"oughtn't": "ought not",
+"oughtn't've": "ought not have",
+"shan't": "shall not",
+"sha'n't": "shall not",
+"shan't've": "shall not have",
+"she'd": "she would",
+"she'd've": "she would have",
+"she'll": "she will",
+"she'll've": "she will have",
+"she's": "she is",
+"should've": "should have",
+"shouldn't": "should not",
+"shouldn't've": "should not have",
+"so've": "so have",
+"so's": "so as",
+"that'd": "that would",
+"that'd've": "that would have",
+"that's": "that is",
+"there'd": "there would",
+"there'd've": "there would have",
+"there's": "there is",
+"they'd": "they would",
+"they'd've": "they would have",
+"they'll": "they will",
+"they'll've": "they will have",
+"they're": "they are",
+"they've": "they have",
+"to've": "to have",
+"wasn't": "was not",
+"we'd": "we would",
+"we'd've": "we would have",
+"we'll": "we will",
+"we'll've": "we will have",
+"we're": "we are",
+"we've": "we have",
+"weren't": "were not",
+"what'll": "what will",
+"what'll've": "what will have",
+"what're": "what are",
+"what's": "what is",
+"what've": "what have",
+"when's": "when is",
+"when've": "when have",
+"where'd": "where did",
+"where's": "where is",
+"where've": "where have",
+"who'll": "who will",
+"who'll've": "who will have",
+"who's": "who is",
+"who've": "who have",
+"why's": "why is",
+"why've": "why have",
+"will've": "will have",
+"won't": "will not",
+"won't've": "will not have",
+"would've": "would have",
+"wouldn't": "would not",
+"wouldn't've": "would not have",
+"y'all": "you all",
+"y'all'd": "you all would",
+"y'all'd've": "you all would have",
+"y'all're": "you all are",
+"y'all've": "you all have",
+"you'd": "you would",
+"you'd've": "you would have",
+"you'll": "you will",
+"you'll've": "you will have",
+"you're": "you are",
+"you've": "you have"
+}
 
 class POSifiedText(markovify.Text):
     def word_split(self, sentence):
@@ -48,23 +176,71 @@ class Markovify(callbacks.Plugin):
         self.__parent = super(Markovify, self)
         self.__parent.__init__(irc)
         self.model = {}
+        self.directory = conf.supybot.directories.data
         self.MATCH_MESSAGE_STRIPNICK = re.compile('^(<[^ ]+> )?(?P<message>.*)$')
 
-    def cleanText(self, user_sentences):
-        fix_text(user_sentences)
-        user_sentences = user_sentences.strip()                         # Strip whitespace from beginning and the end of the string.
-        user_sentences = utils.str.normalizeWhitespace(user_sentences)  # Normalize the whitespace in the string.
-        return text
+    def save_corpus(self, channel):
+        file = self.directory.dirize(channel.lower() + "/markov.json")
+        with open(file, 'w') as outfile:
+            jsondata = self.model[channel].to_json()
+            json.dump(jsondata, outfile)
+
+    def add_text(self, channel, text):
+        text = self.expand_contractions(text)
+        text = re.sub("(^')|('$)|\s'|'\s|[\"(\(\)\[\])]", "", text)
+        try:
+            self.model[channel] = markovify.combine(models=[self.model[channel], POSifiedText(text, retain_original=False)])
+        except KeyError:
+            file = self.directory.dirize(channel.lower() + "/markov.json")
+            try:
+                with open(file) as infile:
+                    jsondata = json.load(infile)
+                    self.model[channel] = POSifiedText.from_json(jsondata)
+                    self.model[channel] = markovify.combine(models=[self.model[channel], POSifiedText(text)])
+            except:
+                self.model[channel] = POSifiedText(text, retain_original=False)
+
+    def get_response(self, channel):
+        try:
+            response = self.model[channel].make_sentence()
+        except KeyError:
+            file = self.directory.dirize(channel.lower() + "/markov.json")
+            try:
+                with open(file) as infile:
+                    jsondata = json.load(infile)
+                    self.model[channel] = POSifiedText.from_json(jsondata)
+            except:
+                return
+            response = self.model[channel].make_sentence()
+        except:
+            return
+        if response and len(response) > 1 and not response.isspace():
+            response = re.sub(' ([.!?,;:]) ', '\g<1> ', response)
+            response = re.sub(' ([.!?,])$', '\g<1>', response)
+            response = re.sub('([.?!,])(?=[^\s])', '\g<1> ', response)
+            response - response.replace(' - ', '-')
+            return response
+        else:
+            return None
+
+    def expand_contractions(self, text, contraction_mapping=CONTRACTION_MAP):
+        contractions_pattern = re.compile('({})'.format('|'.join(contraction_mapping.keys())),
+                                          flags=re.IGNORECASE|re.DOTALL)
+        def expand_match(contraction):
+            match = contraction.group(0)
+            first_char = match[0]
+            expanded_contraction = contraction_mapping.get(match)\
+                                    if contraction_mapping.get(match)\
+                                    else contraction_mapping.get(match.lower())
+            expanded_contraction = first_char+expanded_contraction[1:]
+            return expanded_contraction
+        expanded_text = contractions_pattern.sub(expand_match, text)
+        expanded_text = re.sub("'", "", expanded_text)
+        return expanded_text
 
     def _subreddit(self, subreddit, latest_timestamp=None):
         """
         Downloads the subreddit comments, 500 at a time.
-        Parameters
-        ----------
-        subreddit : str
-            The subreddit name.
-        latest_timestamp : int
-            The latest comment timestamp.
         """
         base_url = "https://api.pushshift.io/reddit/comment/search/"
         params = {"subreddit": subreddit, "sort": "desc",
@@ -74,10 +250,9 @@ class Markovify(callbacks.Plugin):
         with requests.get(base_url, params=params) as response:
             data = response.json()
             self.count += len(data["data"])
-            text = [self.cleanText(item['body']) for item in data["data"]]
             self.latest_timestamp = data['data'][-1]["created_utc"]
-            text = POSifiedText(text)
-            return text
+            data = [fix_text(item['body']) for item in data["data"]]
+            return data
 
     def doPrivmsg(self, irc, msg):
         (channel, message) = msg.args
@@ -96,29 +271,21 @@ class Markovify(callbacks.Plugin):
         if not message and len(message) > 1 or message.isspace():
             return
         if random.random() < probability:
-            try:
-                new_comment = self.model[channel].make_sentence()
-            except KeyError:
-                directory = conf.supybot.directories.data
-                directory = directory.dirize(channel.lower() + "/markov.json")
-                with open(directory, 'r') as infile:
-                    jsondata = json.load(infile)
-                    self.model[channel] = POSifiedText.from_json(jsondata)
-                new_comment = self.model[channel].make_sentence()
-            except:
-                self.model[channel] = POSifiedText("Hello!")
-            if new_comment and len(new_comment) > 1 and not new_comment.isspace():
-                self.model[channel] = markovify.combine(models=[self.model[channel], POSifiedText(message), POSifiedText(new_comment)])
-                irc.reply(new_comment, prefixNick=False)
+            response = self.get_response(channel)
+            if response and len(response) > 1 and not response.isspace():
+                irc.reply(response, prefixNick=False)
+                self.add_text(channel, response)
+                self.add_text(channel, message)
         else:
-            self.model[channel] = markovify.combine(models=[self.model[channel], POSifiedText(message)])
+            self.add_text(channel, message)
+        self.save_corpus(channel)
 
     def processText(self, channel, text):
+        text = fix_text(text)
         match = False
         ignore = self.registryValue("ignorePattern", channel)
         strip = self.registryValue("stripPattern", channel)
         text = ircutils.stripFormatting(text)
-        text = fix_text(text)
         if self.registryValue('stripRelayedNick', channel):
             text = self.MATCH_MESSAGE_STRIPNICK.match(text).group('message')
         if ignore:
@@ -137,69 +304,108 @@ class Markovify(callbacks.Plugin):
             if new_text != text:
                 log.debug("Markovify: url(s) stripped from text for %s. New text text: %s" % (channel, new_text))
                 text = new_text
-        text = text.strip()                         # Strip whitespace from beginning and the end of the string.
-        text = utils.str.normalizeWhitespace(text)  # Normalize the whitespace in the string.
-        text = self.capsents(text)
+        ends_with_punctuation = False
+        for char in [".", "?", "!"]:
+            if text[-1] == char:
+                ends_with_punctuation = True
+                break
+        if not ends_with_punctuation:
+            text = text + "."
         if text and len(text) > 1 and not text.isspace():
             return text
         else:
             return None
 
-    def subreddit(self, irc, msg, args, channel, subreddits):
-        """[channel] <subreddit_1> [subreddit_2] [subreddit_3] [...etc.]
-        Load subreddit comments into csv files
+    def subreddit(self, irc, msg, args, channel, optlist, subreddits):
+        """[channel] [--num <###>] <subreddit_1> [subreddit_2] [subreddit_3] [...etc.]
+        Load subreddit comments into channel corpus.
         """
-        if not channel: # Did the user enter in a channel? If not, set the currect channel
+        if not channel:
             channel = msg.args[0]
         channel = msg.args[0].lower()
+        optlist = dict(optlist)
+        if 'num' in optlist:
+            max_comments = optlist.get('num')
+        else:
+            max_comments = 500
         for subreddit in subreddits.lower().strip().split(' '):
             self.latest_timestamp = None
-            max_comments = 2000
-            irc.reply("Downloading: {}".format(subreddit))
+            irc.reply("Attempting to retrieve last {0} comments from r/{1}".format(max_comments, subreddit))
+            self.count = 0
+            data = self._subreddit(subreddit, self.latest_timestamp)
+            text = ""
             tries = 0
             self.count = 0
-            self.model[channel] = None
-            while self.count <= max_comments:
-                if tries >= 50:
+            data = []
+            while self.count < max_comments:
+                if tries > 20:
                     break
-                if self.model[channel]:
-                    self.model[channel] = markovify.combine(models=[self.model[channel], self._subreddit(subreddit, self.latest_timestamp)])
-                else:
-                    self.model[channel] = self._subreddit(subreddit, self.latest_timestamp)
+                data.extend(self._subreddit(subreddit, self.latest_timestamp))
                 tries += 1
-            directory = conf.supybot.directories.data
-            directory = directory.dirize(channel.lower() + "/markov.json")
-            with open(directory, 'w') as outfile:
-                jsondata = self.model[channel].to_json()
-                json.dump(jsondata, outfile)
-            irc.reply("Retrieved {0} comments from {1}".format(self.count, subreddit))
-    subreddit = wrap(subreddit, [additional('channel'), 'text'])
+            if data:
+                for line in data:
+                    if '[removed]' in line:
+                        continue
+                    ends_with_punctuation = False
+                    for char in [".", "?", "!"]:
+                        if line[-1] == char:
+                            ends_with_punctuation = True
+                            break
+                    if not ends_with_punctuation:
+                        line = line + "."
+                    text += " {}".format(line)
+                self.add_text(channel, text)
+            self.save_corpus(channel)
+            irc.reply("Added {0} comments from r/{1}.".format(self.count, subreddit))
+            del data, text
+            gc.collect()
+    subreddit = wrap(subreddit, [additional('channel'), getopts({'num':'int'}), 'text'])
+
+    def text(self, irc, msg, args, channel, optlist, url):
+        """[channel] <url>
+        Load text file into channel corpus.
+        """
+        if not channel:
+            channel = msg.args[0]
+        channel = msg.args[0].lower()
+        optlist = dict(optlist)
+        if 'process' in optlist:
+            process = True
+        else:
+            process = False
+        r = requests.head(url)
+        if "text/plain" in r.headers["content-type"]:
+            file = requests.get(url)
+        else:
+            irc.reply("Invalid file type.", private=False, notice=False)
+            return
+        data = fix_text(file.content.decode())
+        lines = 0
+        for line in data.split('\n'):
+            if process:
+                line = self.processText(channel,line)
+            if line.strip():
+                text += " {}".format(line)
+                lines += 1
+            else:
+                continue
+        self.add_text(channel, text)
+        irc.reply("{0} lines added to brain file for channel {1}.".format(lines, channel))
+        del data
+        gc.collect()
+    text = wrap(text, [additional('channel'), getopts({'process':''}), 'text'])
 
     def respond(self, irc, msg, args, channel):
-        """[channel] <text>
-        Respomd to <text> using channel conversational model
+        """[channel]
+        Generate a response from channel corpus.
         """
         if not channel:
             channel = msg.args[0]
         channel = channel.lower()
-        try:
-            new_comment = self.model[channel].make_sentence()
-        except KeyError:
-            directory = conf.supybot.directories.data
-            directory = directory.dirize(channel.lower() + "/markov.json")
-            with open(directory) as infile:
-                jsondata = json.load(infile)
-                self.model[channel] = POSifiedText.from_json(jsondata)
-            new_comment = self.model[channel].make_sentence()
-        except:
-            return
-        irc.reply(new_comment, prefixNick=False)
-        self.model[channel] = markovify.combine(models=[self.model[channel], POSifiedText(new_comment)])
-        directory = conf.supybot.directories.data
-        directory = directory.dirize(channel.lower() + "/markov.json")
-        with open(directory, 'w') as outfile:
-            jsondata = self.model[channel].to_json()
-            json.dump(jsondata, outfile)
+        response = self.get_response(channel)
+        if response:
+            irc.reply(response, prefixNick=False)
+            self.save_corpus(channel)
     respond = wrap(respond, [optional('channel')])
 
 Class = Markovify
