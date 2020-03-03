d67da5d738cd9f6cbca4234cacf811f751180d73
oddluck <39967334+oddluck@users.noreply.github.com>
2020-03-03 22:27:01 +0000

    Corona: fix template colors

--- Corona/config.py
+++ Corona/config.py
@@ -50,4 +50,4 @@ def configure(advanced):
 Corona = conf.registerPlugin('Corona')
 
 conf.registerChannelValue(Corona, 'template',
-     registry.String("\x02$location: \x0309$confirmed\x03 infected, \x0304$dead\x03 dead ($ratio), \x0309$recovered\x03 recovered.", _("""Template for replies""")))
+     registry.String("\x02$location: \x0307$confirmed\x03 infected, \x0304$dead\x03 dead ($ratio), \x0309$recovered\x03 recovered.", _("""Template for replies""")))
