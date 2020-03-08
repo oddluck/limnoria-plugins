6fb12187e768e2f410e50d68f2160af067a8ccdc
oddluck <39967334+oddluck@users.noreply.github.com>
2020-03-08 16:27:35 +0000

    GoogleCloud: add plugin

---
+++ GoogleCloud/README.md
@@ -0,0 +1,7 @@
+Access various Google Cloud APIs (translate).
+
+## GoogleCloud Plugin
+
+Enable the [Cloud Translation API](https://console.cloud.google.com/apis/library/translate.googleapis.com). Set your [API Key](https://console.cloud.google.com/apis/credentials) using the command below.
+
+`config plugins.GoogleCloud.translate.key` Set your key for the text translation API
---
+++ GoogleCloud/plugin.py
@@ -0,0 +1,72 @@
+###
+# Copyright (c) 2020, oddluck <oddluck@riseup.net>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#   * Redistributions of source code must retain the above copyright notice,
+#     this list of conditions, and the following disclaimer.
+#   * Redistributions in binary form must reproduce the above copyright notice,
+#     this list of conditions, and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#   * Neither the name of the author of this software nor the name of
+#     contributors to this software may be used to endorse or promote products
+#     derived from this software without specific prior written consent.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+###
+
+
+import supybot.utils as utils
+from supybot.commands import *
+import supybot.plugins as plugins
+import supybot.ircutils as ircutils
+import supybot.callbacks as callbacks
+import supybot.conf as conf
+import requests
+import json
+
+class GoogleCloud(callbacks.Plugin):
+
+
+    def translate(self, irc, msg, args, optlist, text):
+        """[--from <source>] [--to <target>] <text>
+        Translate text using Google Translate API. Uses automatic language detection if source not
+        set. No target uses the plugin default.
+        """
+        optlist = dict(optlist)
+        key = self.registryValue('translate.key')
+        if 'from' in optlist:
+            source = optlist.get('from')
+        else:
+            source = self.registryValue('translate.source')
+        if 'to' in optlist:
+            target = optlist.get('to')
+        else:
+            target = self.registryValue('translate.target')
+        if source != 'auto':
+            url = 'https://translation.googleapis.com/language/translate/v2?q={0}&target={1}&source={2}&key={3}'.format(text, target, source, key)
+        else:
+            url = 'https://translation.googleapis.com/language/translate/v2?q={0}&target={1}&key={2}'.format(text, target, key)
+        response = requests.get(url, timeout=10)
+        result = json.loads(response.content)
+        if result['data']['translations'][0].get('translatedText'):
+            reply = "{0} [{1}~>{2}]".format(result['data']['translations'][0]['translatedText'], result['data']['translations'][0]['detectedSourceLanguage'], target)
+        else:
+            reply = "{0} [{1}~>{2}]".format(result['data']['translations'][0]['translatedText'], source, target)
+        irc.reply(reply)
+    translate = wrap(translate, [getopts({'from':'text', 'to':'text'}), 'text'])
+
+
+Class = GoogleCloud
