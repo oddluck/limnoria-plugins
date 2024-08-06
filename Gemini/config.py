db7451ad9d9446652de0986c095cc57a323f1354
Gordon Shumway <fiendinacloud@gmail.com>
2024-08-05 20:39:58 -0400

    Add Gemini plugin

---
+++ Gemini/config.py
@@ -0,0 +1,146 @@
+###
+# Copyright (c) 2024, oddluck
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#   * Redistributions of source code must retain the above copyright notice,
+#     this list of conditions, and the following disclaimer.
+#   * Redistributions in binary form must reproduce the above copyright notice,
+#     this list of conditions, and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#   * Neither the name of the author of this software nor the name of
+#     contributors to this software may be used to endorse or promote products
+#     derived from this software without specific prior written consent.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+###
+
+from supybot import conf, registry
+
+try:
+    from supybot.i18n import PluginInternationalization
+
+    _ = PluginInternationalization("Gemini")
+except:
+    # Placeholder that allows to run the plugin on a bot
+    # without the i18n module
+    _ = lambda x: x
+
+
+def configure(advanced):
+    # This will be called by supybot to configure this module.  advanced is
+    # a bool that specifies whether the user identified themself as an advanced
+    # user or not.  You should effect your configuration by manipulating the
+    # registry as appropriate.
+    from supybot.questions import expect, anything, something, yn
+
+    conf.registerPlugin("Gemini", True)
+
+
+Gemini = conf.registerPlugin("Gemini")
+# This is where your configuration variables (if any) should go.  For example:
+# conf.registerGlobalValue(Gemini, 'someConfigVariableName',
+#     registry.Boolean(False, _("""Help for someConfigVariableName.""")))
+
+conf.registerChannelValue(
+    Gemini,
+    "api_key",
+    registry.String(
+        "",
+        _("""Your GoogleAI API Key (required)"""),
+        private=True,
+    ),
+)
+
+conf.registerChannelValue(
+    Gemini,
+    "model",
+    registry.String(
+        "gemini-1.5-flash-latest",
+        _(
+            """
+            GoogleAI endpoint model, default: "gemini-1.5-flash-latest"
+            """
+        ),
+    ),
+)
+
+conf.registerChannelValue(
+    Gemini,
+    "prompt",
+    registry.String(
+        "You are $botnick the IRC bot. Be brief, helpful. Keep each line of reply 400 characters or less. Do not prefix replies with your name.",
+        _(
+            """
+            The prompt defining your bot's personality.
+            """
+        ),
+    ),
+)
+
+conf.registerChannelValue(
+    Gemini,
+    "max_tokens",
+    registry.Integer(
+        200,
+        _(
+            """
+            The maximum number of tokens to generate in the response
+            """
+        ),
+    ),
+)
+
+conf.registerChannelValue(
+    Gemini,
+    "max_history",
+    registry.Integer(
+        50,
+        _(
+            """
+            The maximum number of messages to keep in conversation history.
+            """
+        ),
+    ),
+)
+
+conf.registerChannelValue(
+    Gemini,
+    "nick_include",
+    registry.Boolean(
+        True,
+        _(
+            """
+            Include user nicks in history/queries. Disabled will treat conversation as if from a single user.
+            """
+        ),
+    ),
+)
+
+conf.registerChannelValue(
+    Gemini,
+    "nick_prefix",
+    registry.Boolean(
+        False,
+        _(
+            """
+            Prefix nick on replies true/false...
+            """
+        ),
+    ),
+)
+
+# vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
---
+++ Gemini/plugin.py
@@ -0,0 +1,89 @@
+###
+# Copyright (c) 2024, oddluck
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#   * Redistributions of source code must retain the above copyright notice,
+#     this list of conditions, and the following disclaimer.
+#   * Redistributions in binary form must reproduce the above copyright notice,
+#     this list of conditions, and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#   * Neither the name of the author of this software nor the name of
+#     contributors to this software may be used to endorse or promote products
+#     derived from this software without specific prior written consent.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+###
+
+from supybot import utils, plugins, ircutils, callbacks
+from supybot.commands import *
+from supybot.i18n import PluginInternationalization
+import google.generativeai as genai
+
+_ = PluginInternationalization("Gemini")
+
+
+class Gemini(callbacks.Plugin):
+    """GoogleAI Gemini Chat Plugin"""
+
+    threaded = True
+
+    def __init__(self, irc):
+        self.__parent = super(Gemini, self)
+        self.__parent.__init__(irc)
+        self.history = {}
+
+    def chat(self, irc, msg, args, text):
+        """Chat Call to the Gemini API"""
+        channel = msg.channel
+        if not irc.isChannel(channel):
+            channel = msg.nick
+        api_key = self.registryValue("api_key", msg.channel)
+        genai.configure(api_key=api_key)
+        max_tokens = self.registryValue("max_tokens", msg.channel)
+        max_history = self.registryValue("max_history", msg.channel)
+        generation_config = {"max_output_tokens": max_tokens}
+        prompt = self.registryValue("prompt", msg.channel).replace("$botnick", irc.nick)
+        ai_model = self.registryValue("model", msg.channel)
+        prefix = self.registryValue("nick_prefix", msg.channel)
+        include = self.registryValue("nick_include", msg.channel)
+        model = genai.GenerativeModel(
+            ai_model,
+            generation_config=generation_config,
+            system_instruction=prompt,
+        )
+        self.history.setdefault(channel, None)
+        if not self.history[channel]:
+            self.history[channel] = []
+        if len(self.history[channel]) > max_history:
+            self.history[channel] = []
+        chat = model.start_chat(history=self.history[channel][-max_history:])
+        if include:
+            response = chat.send_message("%s: %s" % (msg.nick, text))
+        else:
+            response = chat.send_message(text)
+        for line in response.text.splitlines():
+            if line:
+                irc.reply(line, prefixNick=prefix)
+        self.history[channel] = chat.history
+
+    chat = wrap(chat, ["text"])
+
+
+Class = Gemini
+
+
+# vim:set shiftwidth=4 softtabstop=4 expandtab textwidth=79:
